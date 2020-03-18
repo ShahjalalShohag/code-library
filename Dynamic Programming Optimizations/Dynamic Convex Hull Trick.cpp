6a6198b12e548cf12b981545ef30aeffacb489f7
ShahjalalShohag <33003206+ShahjalalShohag@users.noreply.github.com>
2020-03-18 17:46:49 +0600

    Add files via upload

---
+++ Dynamic Programming Optimizations/Dynamic Convex Hull Trick.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define eb emplace_back
+#define nl '\n'
+#define deb(x) cerr << #x" = " << x << nl
+#define in() ( { int a ; scanf("%d", &a); a; } )
+
+const int N = 3e5 + 9;
+const int mod = 1e9 + 7;
+
+//add lines with -m and -b and return -ans to
+//make this code working for minimums.(not -x)
+const ll inf = -(1LL<<62);
+struct line {
+    ll m, b;
+    mutable function<const line*() > succ;
+    bool operator < (const line& rhs) const {
+        if (rhs.b != inf) return m < rhs.m;
+        const line* s = succ();
+        if (!s) return 0;
+        ll x = rhs.m;
+        return b - s->b < (s->m - m) * x;
+    }
+};
+struct CHT : public multiset<line> {
+    bool bad(iterator y) {
+        auto z = next(y);
+        if (y == begin()) {
+            if (z == end()) return 0;
+            return y -> m == z -> m && y -> b <= z -> b;
+        }
+        auto x = prev(y);
+        if (z == end()) return y -> m == x -> m && y -> b <= x -> b;
+        return 1.0 * (x -> b - y -> b)*(z -> m - y -> m) >= 1.0 * (y -> b - z -> b)*(y -> m - x -> m);
+    }
+    void add(ll m, ll b) {
+        auto y = insert({ m, b });
+        y->succ = [=] { return next(y) == end() ? 0 : &*next(y); };
+        if (bad(y)) { erase(y); return; }
+        while (next(y) != end() && bad(next(y))) erase(next(y));
+        while (y != begin() && bad(prev(y))) erase(prev(y));
+    }
+    ll query(ll x) {
+        auto l = *lower_bound((line) { x, inf });
+        return l.m * x + l.b;
+    }
+};
+ll a[N],b[N];
+CHT* cht;
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n; cin >> n;
+    for(int i = 0; i < n; i++) cin >> a[i];
+    for(int i = 0; i < n; i++) cin >> b[i];
+    cht = new CHT();
+    cht -> add(-b[0], 0);
+    ll ans = 0;
+    for(int i = 1; i < n; i++){
+        ans = -cht -> query(a[i]);
+        cht -> add(-b[i], -ans);
+    }
+    cout << ans << nl;
+    return 0;
+}
---
+++ Dynamic Programming Optimizations/Li Chao Tree.cpp
@@ -0,0 +1,91 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define eb emplace_back
+#define nl '\n'
+#define deb(x) cerr << #x" = " << x << nl
+#define in() ( { int a ; scanf("%d", &a); a; } )
+
+const int N = 1e5 + 9;
+const int mod = 1e9 + 7;
+
+struct Line {
+    ll k, d;
+    ll eval(int x) {
+        return k * x + d;
+    }
+};
+
+struct LiChaoNode {
+    Line line;
+    LiChaoNode* left = nullptr;
+    LiChaoNode* right = nullptr;
+
+    LiChaoNode(Line line) : line(line) {}
+
+    void add_line(Line nw, int l, int r) {
+        int m = (l + r) / 2;
+        bool lef = nw.eval(l) < line.eval(l);
+        bool mid = nw.eval(m) < line.eval(m);
+        if(mid) swap(line, nw);
+        if(l == r) return;
+        if(lef != mid) {
+            if(left == nullptr) left = new LiChaoNode(nw);
+            else left -> add_line(nw, l, m);
+        }
+        else{
+            if(right == nullptr) right = new LiChaoNode(nw);
+            else right -> add_line(nw, m + 1, r);
+        }
+    }
+
+    ll query(int x, int l, int r) {
+        ll val = line.eval(x);
+        int m = (l + r) / 2;
+        if(l < r) {
+            if(x <= m && left != nullptr) val = min(val, left -> query(x, l, m));
+            if(x > m && right != nullptr) val = min(val, right -> query(x, m + 1, r));
+        }
+        return val;
+    }
+};
+
+struct LiChaoTree
+{
+    int L, R;
+    LiChaoNode* root;
+
+    LiChaoTree() : L(numeric_limits<int>::min() / 2), R(numeric_limits<int>::max() / 2), root(nullptr) {}
+    LiChaoTree(int L, int R) : L(L), R(R) {
+        root = new LiChaoNode({0, numeric_limits<ll>::max() / 2});
+    }
+
+    void add_line(Line line) {
+        root -> add_line(line, L, R);
+    }
+    ll query(int x) {
+        return root -> query(x, L, R);
+    }
+};
+
+
+int a[N], b[N];
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n; cin >> n;
+    for(int i = 0; i < n; i++) cin >> a[i];
+    for(int i = 0; i < n; i++) cin >> b[i];
+    LiChaoTree t = LiChaoTree(0, (int) 1e9);
+    t.add_line({b[0], 0});
+    ll ans = 0;
+    for(int i = 1; i < n; i++){
+        ans = t.query(a[i]);
+        t.add_line({b[i], ans});
+    }
+    cout << ans << nl;
+    return 0;
+}

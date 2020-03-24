87e3f6f5057e8f9af25d3cc342f64108e9ea3bfe
ShahjalalShohag <33003206+ShahjalalShohag@users.noreply.github.com>
2020-03-24 21:38:44 +0600

    Add files via upload

---
+++ Graph Theory/Articulation Points.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 3e5 + 9;
+
+int T, low[N], dis[N], art[N];
+vector<int> g[N];
+void dfs(int u, int pre = 0)
+{
+    low[u] = dis[u] = ++T;
+    int child = 0;
+    for(auto v: g[u]){
+        if(!dis[v]){
+            dfs(v, u);
+            low[u] = min(low[u], low[v]);
+            if(low[v] >= dis[u] && pre != 0) art[u] = 1;
+            ++child;
+        }
+        else if(v != pre) low[u] = min(low[u], dis[v]);
+    }
+    if(pre == 0 && child > 1) art[u] = 1;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    while(1){
+        int n, m; cin >> n >> m;
+        if(!n) break;
+        while(m--){
+            int u, v; cin >> u >> v;
+            g[u].push_back(v);
+            g[v].push_back(u);
+        }
+        dfs(1);
+        int ans = 0;
+        for(int i = 1; i <= n; i++) ans += art[i];
+        cout << ans << '\n';
+        T = 0; for(int i = 1; i <= n; i++) low[i] = dis[i] = art[i] = 0, g[i].clear();
+    }
+    return 0;
+}
---
+++ Graph Theory/Block Cut Tree.cpp
@@ -0,0 +1,91 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 4e5 + 9;
+
+int T, low[N], dis[N], art[N], sz;
+vector<int> g[N], bcc[N], st;
+void dfs(int u, int pre = 0)
+{
+    low[u] = dis[u] = ++T;
+    st.push_back(u);
+    for(auto v: g[u]){
+        if(!dis[v]){
+            dfs(v, u);
+            low[u] = min(low[u], low[v]);
+            if(low[v] >= dis[u]){
+                sz ++;
+                int x;
+                do{
+                    x = st.back(); st.pop_back();
+                    bcc[x].push_back(sz);
+                }while(x ^ v);
+                bcc[u].push_back(sz);
+            }
+        }
+        else if(v != pre) low[u] = min(low[u], dis[v]);
+    }
+}
+
+int dep[N], par[N][20], cnt[N], id[N];
+vector<int> bt[N];
+void dfs1(int u, int pre = 0)
+{
+    dep[u] = dep[pre] + 1;
+    cnt[u] = cnt[pre] + art[u];
+    par[u][0] = pre;
+    for(int k = 1; k <= 18; k++) par[u][k] = par[par[u][k - 1]][k - 1];
+    for(auto v: bt[u]) if(v != pre) dfs1(v, u);
+}
+
+int lca(int u, int v)
+{
+    if(dep[u] < dep[v]) swap(u, v);
+    for(int k = 18; k >= 0; k--) if(dep[par[u][k]] >= dep[v]) u = par[u][k];
+    if(u == v) return u;
+    for(int k = 18; k >= 0; k--) if(par[u][k] != par[v][k]) u = par[u][k], v = par[v][k];
+    return par[u][0];
+}
+
+int dist(int u, int v)
+{
+    int lc = lca(u, v);
+    return cnt[u] + cnt[v] - 2 * cnt[lc] + art[lc];
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, m; cin >> n >> m;
+    while(m--){
+        int u, v; cin >> u >> v;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+    dfs(1);
+    for(int u = 1; u <= n; u++){
+        if(bcc[u].size() > 1){ //AP
+            id[u] = ++sz;
+            art[id[u]] = 1; //if id in BCT is an AP on real graph or not
+            for(auto v: bcc[u]){
+                bt[id[u]].push_back(v);
+                bt[v].push_back(id[u]);
+            }
+        }
+        else if(bcc[u].size() == 1) id[u] = bcc[u][0];
+    }
+
+    dfs1(1);
+    int q; cin >> q;
+    while(q--){
+        int u, v; cin >> u >> v;
+        int ans;
+        if(u == v) ans = 0;
+        else ans = dist(id[u], id[v]) - art[id[u]] - art[id[v]];
+        cout << ans << '\n';;//number of articulation points in the path from u to v except u and v
+        //u and v are in the same bcc if ans == 0
+    }
+    return 0;
+}

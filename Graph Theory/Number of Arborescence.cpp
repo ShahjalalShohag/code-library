11ab7df1a43e8e4ccdcb0ee1039d7886d796c927
ShahjalalShohag <33003206+ShahjalalShohag@users.noreply.github.com>
2020-03-28 04:26:24 +0600

    Add files via upload

---
+++ Graph Theory/Number of Arborescence.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 1e3 + 9, mod = 1e9 + 7;
+
+template <int32_t MOD>
+struct modint {
+    int32_t value;
+    modint() = default;
+    modint(int32_t value_) : value(value_) {}
+    inline modint<MOD> operator + (modint<MOD> other) const { int32_t c = this->value + other.value; return modint<MOD>(c >= MOD ? c - MOD : c); }
+    inline modint<MOD> operator - (modint<MOD> other) const { int32_t c = this->value - other.value; return modint<MOD>(c <    0 ? c + MOD : c); }
+    inline modint<MOD> operator * (modint<MOD> other) const { int32_t c = (int64_t)this->value * other.value % MOD; return modint<MOD>(c < 0 ? c + MOD : c); }
+    inline modint<MOD> & operator += (modint<MOD> other) { this->value += other.value; if (this->value >= MOD) this->value -= MOD; return *this; }
+    inline modint<MOD> & operator -= (modint<MOD> other) { this->value -= other.value; if (this->value <    0) this->value += MOD; return *this; }
+    inline modint<MOD> & operator *= (modint<MOD> other) { this->value = (int64_t)this->value * other.value % MOD; if (this->value < 0) this->value += MOD; return *this; }
+    inline modint<MOD> operator - () const { return modint<MOD>(this->value ? MOD - this->value : 0); }
+    modint<MOD> pow(uint64_t k) const {
+        modint<MOD> x = *this, y = 1;
+        for (; k; k >>= 1) {
+            if (k & 1) y *= x;
+            x *= x;
+        }
+        return y;
+    }
+    modint<MOD> inv() const { return pow(MOD - 2); }  // MOD must be a prime
+    inline modint<MOD> operator /  (modint<MOD> other) const { return *this *  other.inv(); }
+    inline modint<MOD> operator /= (modint<MOD> other)       { return *this *= other.inv(); }
+    inline bool operator == (modint<MOD> other) const { return value == other.value; }
+    inline bool operator != (modint<MOD> other) const { return value != other.value; }
+};
+template <int32_t MOD> modint<MOD> operator * (int64_t value, modint<MOD> n) { return modint<MOD>(value) * n; }
+template <int32_t MOD> modint<MOD> operator * (int32_t value, modint<MOD> n) { return modint<MOD>(value % MOD) * n; }
+template <int32_t MOD> ostream & operator << (ostream & out, modint<MOD> n) { return out << n.value; }
+
+using mint = modint<mod>;
+
+//number of arborescences i.e. directed rooted trees = unlabeled rooted connected trees with n nodes, isomorphic trees will be counted once
+//a(n+1) = (1/n) * Sum_{k=1..n} ( Sum_{d|k} d*a(d) ) * a(n-k+1)
+mint dp[N];
+vector<int> d[N];
+int32_t main()
+{
+    for(int i = 1; i < N; i++) for(int j = i; j < N; j += i) d[j].push_back(i);
+    dp[0] = 0; dp[1] = 1;
+    for(int i = 2; i < N; i++){
+        dp[i] = 0;
+        mint sum = 0;
+        for(int k = 1; k < i; k++){
+            mint sum = 0;
+            for(auto x: d[k]) sum += dp[x] * x;
+            sum *= dp[i - k];
+            dp[i] += sum;
+        }
+        dp[i] *= mint(i - 1).inv();
+    }
+    for(int i = 1; i <= 20; i++) cout << dp[i] << ' ';
+    return 0;
+}

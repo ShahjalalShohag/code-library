ba4ea1a5528d40a1a987f2cb3557d602aa6d89e3
ShahjalalShohag <33003206+ShahjalalShohag@users.noreply.github.com>
2020-04-02 01:12:40 +0600

    Update Tree Isomorphism.cpp

--- Graph Theory/Tree Isomorphism.cpp
+++ Graph Theory/Tree Isomorphism.cpp
@@ -1,9 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-const int N = 3e5 + 9, mod = 998244353;
+const int N = 3e5 + 9, mod = 1e9 + 97;
+template <int32_t MOD>
+struct modint {
+    int32_t value;
+    modint() = default;
+    modint(int32_t value_) : value(value_) {}
+    inline modint<MOD> operator + (modint<MOD> other) const { int32_t c = this->value + other.value; return modint<MOD>(c >= MOD ? c - MOD : c); }
+    inline modint<MOD> operator - (modint<MOD> other) const { int32_t c = this->value - other.value; return modint<MOD>(c <    0 ? c + MOD : c); }
+    inline modint<MOD> operator * (modint<MOD> other) const { int32_t c = (int64_t)this->value * other.value % MOD; return modint<MOD>(c < 0 ? c + MOD : c); }
+    inline modint<MOD> & operator += (modint<MOD> other) { this->value += other.value; if (this->value >= MOD) this->value -= MOD; return *this; }
+    inline modint<MOD> & operator -= (modint<MOD> other) { this->value -= other.value; if (this->value <    0) this->value += MOD; return *this; }
+    inline modint<MOD> & operator *= (modint<MOD> other) { this->value = (int64_t)this->value * other.value % MOD; if (this->value < 0) this->value += MOD; return *this; }
+    inline modint<MOD> operator - () const { return modint<MOD>(this->value ? MOD - this->value : 0); }
+    modint<MOD> pow(uint64_t k) const {
+        modint<MOD> x = *this, y = 1;
+        for (; k; k >>= 1) {
+            if (k & 1) y *= x;
+            x *= x;
+        }
+        return y;
+    }
+    modint<MOD> inv() const { return pow(MOD - 2); }  // MOD must be a prime
+    inline modint<MOD> operator /  (modint<MOD> other) const { return *this *  other.inv(); }
+    inline modint<MOD> operator /= (modint<MOD> other)       { return *this *= other.inv(); }
+    inline bool operator == (modint<MOD> other) const { return value == other.value; }
+    inline bool operator != (modint<MOD> other) const { return value != other.value; }
+    inline bool operator < (modint<MOD> other) const { return value < other.value; }
+    inline bool operator > (modint<MOD> other) const { return value > other.value; }
+};
+template <int32_t MOD> modint<MOD> operator * (int64_t value, modint<MOD> n) { return modint<MOD>(value) * n; }
+template <int32_t MOD> modint<MOD> operator * (int32_t value, modint<MOD> n) { return modint<MOD>(value % MOD) * n; }
+template <int32_t MOD> ostream & operator << (ostream & out, modint<MOD> n) { return out << n.value; }
 
-int pw[N];
+using mint = modint<mod>;
+mint pw[N];
+const mint P = 998244353, Q = 1e9 + 33 , R = 99999989;
 const int base = 10;
 struct Tree
 {
@@ -43,19 +76,16 @@ struct Tree
         }
         return ans;
     }
-    pair<int, int> yo(int u, int pre = 0)
+    mint yo(int u, int pre = 0)
     {
-        vector<pair<int, int>> nw;
+        vector<mint> nw;
         for(auto v: g[u]) if(v != pre) nw.push_back(yo(v, u));
-        sort(nw.begin(), nw.end());
-        int ans = 0; int len = 0;
+        mint ans = 0;
         for(auto x: nw){
-            int v = x.first; int l = x.second;
-            ans = 1LL * ans * pw[l] % mod; ans += v; if(ans >= mod) ans -= mod;
-            len += l;
+            ans = ans + P.pow(x.value);
         }
-        ans += pw[len]; ans = 1LL * ans * 10 % mod; len += 2;
-        return {ans, len};
+        ans = ans * Q + R;
+        return ans;
     }
     bool iso(Tree &t)
     {
@@ -70,8 +100,6 @@ int32_t main()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    pw[0] = 1;
-    for(int i = 1; i < N; i++) pw[i] = 1LL * pw[i - 1] * base % mod;
     int t; cin >> t;
     while(t--){
         int n; cin >> n;

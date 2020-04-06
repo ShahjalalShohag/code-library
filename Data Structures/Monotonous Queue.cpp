c1ecf30d1db7f77df94850b8555dcd79b157fb4d
ShahjalalShohag <33003206+ShahjalalShohag@users.noreply.github.com>
2020-04-06 23:29:16 +0600

    Add files via upload

---
+++ Data Structures/Monotonous Queue.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 3e5 + 9;
+
+struct monotonous_queue //max, stores strictly decreasing sequence of the current queue
+{
+    int a[N + 10], b[N + 10], l = 0, r = -1;
+    void push(int val)
+    {
+        int cnt = 0;
+        while(l <= r && a[r] <= val){
+            cnt += b[r] + 1;
+            r--;
+        }
+        a[++r] = val, b[r] = cnt;
+    };
+    int top()
+    {
+        return a[l];
+    }
+    void pop()
+    {
+        if(l > r) return;
+        if (b[l] > 0){
+            b[l] --;
+            return;
+        }
+        l++;
+    }
+};
+
+int32_t main()
+{
+
+    return 0;
+}

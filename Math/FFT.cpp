8a3c49d7c9c52b6564e1cd9813fef5439aa26edc
ShahjalalShohag <33003206+ShahjalalShohag@users.noreply.github.com>
2020-04-21 02:45:13 +0600

    Add files via upload

---
+++ Math/FFT.cpp
@@ -0,0 +1,77 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 3e5 + 9;
+
+const double PI = acos(-1);
+struct base {
+	double a, b;
+	base(double a = 0, double b = 0) : a(a), b(b) {}
+	const base operator + (const base &c) const
+		{ return base(a + c.a, b + c.b); }
+	const base operator - (const base &c) const
+		{ return base(a - c.a, b - c.b); }
+	const base operator * (const base &c) const
+		{ return base(a * c.a - b * c.b, a * c.b + b * c.a); }
+};
+void fft(vector<base> &p, bool inv = 0) {
+	int n = p.size(), i = 0;
+	for(int j = 1; j < n - 1; ++j) {
+		for(int k = n >> 1; k > (i ^= k); k >>= 1);
+		if(j < i) swap(p[i], p[j]);
+	}
+	for(int l = 1, m; (m = l << 1) <= n; l <<= 1) {
+		double ang = 2 * PI / m;
+		base wn = base(cos(ang), (inv ? 1. : -1.) * sin(ang)), w;
+		for(int i = 0, j, k; i < n; i += m) {
+			for(w = base(1, 0), j = i, k = i + l; j < k; ++j, w = w * wn) {
+				base t = w * p[j + l];
+				p[j + l] = p[j] - t;
+				p[j] = p[j] + t;
+			}
+		}
+	}
+	if(inv) for(int i = 0; i < n; ++i) p[i].a /= n, p[i].b /= n;
+}
+vector<long long> multiply(vector<int> &a, vector<int> &b) {
+	int n = a.size(), m = b.size(), t = n + m - 1, sz = 1;
+	while(sz < t) sz <<= 1;
+	vector<base> x(sz), y(sz), z(sz);
+	for(int i = 0 ; i < sz; ++i) {
+		x[i] = i < (int)a.size() ? base(a[i], 0) : base(0, 0);
+		y[i] = i < (int)b.size() ? base(b[i], 0) : base(0, 0);
+	}
+	fft(x), fft(y);
+	for(int i = 0; i < sz; ++i) z[i] = x[i] * y[i];
+	fft(z, 1);
+	vector<long long> ret(sz);
+	for(int i = 0; i < sz; ++i) ret[i] = (long long) (z[i].a + 0.5);
+	while((int)ret.size() > 1 && ret.back() == 0) ret.pop_back();
+	return ret;
+}
+
+long long ans[N];
+int32_t main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n, x; cin >> n >> x;
+    vector<int> a(n + 1, 0), b(n + 1, 0), c(n + 1, 0);
+    int nw = 0;
+    a[0]++; b[n]++;
+    long long z = 0;
+    for (int i = 1; i <= n; i++) {
+    	int k; cin >> k;
+    	nw += k < x;
+    	a[nw]++; b[-nw + n]++;
+    	z += c[nw] + !nw; c[nw]++;
+    }
+    auto res = multiply(a, b);
+    for (int i = n + 1; i < res.size(); i++) {
+    	ans[i - n] += res[i];
+    }
+    ans[0] = z;
+    for (int i = 0; i <= n; i++) cout << ans[i] << ' ';
+    cout << '\n';
+    return 0;
+}
+//https://codeforces.com/contest/993/problem/E
---
+++ Math/NTT.cpp
@@ -0,0 +1,103 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 1 << 18;
+const int mod = 998244353;
+const int root = 3;
+int lim, rev[N], w[N], wn[N], inv_lim;
+void reduce(int &x) { x = (x + mod) % mod; }
+int POW(int x, int y, int ans = 1) {
+	for (; y; y >>= 1, x = (long long) x * x % mod) if (y & 1) ans = (long long) ans * x % mod;
+	return ans;
+}
+void precompute(int len) {
+	lim = wn[0] = 1; int s = -1;
+    while (lim < len) lim <<= 1, ++s;
+	for (int i = 0; i < lim; ++i) rev[i] = rev[i >> 1] >> 1 | (i & 1) << s;
+	const int g = POW(root, (mod - 1) / lim);
+	inv_lim = POW(lim, mod - 2);
+	for (int i = 1; i < lim; ++i) wn[i] = (long long) wn[i - 1] * g % mod;
+}
+void ntt(vector<int> &a, int typ) {
+	for (int i = 0; i < lim; ++i) if (i < rev[i]) swap(a[i], a[rev[i]]);
+	for (int i = 1; i < lim; i <<= 1) {
+		for (int j = 0, t = lim / i / 2; j < i; ++j) w[j] = wn[j * t];
+		for (int j = 0; j < lim; j += i << 1)
+			for (int k = 0; k < i; ++k) {
+				const int x = a[k + j], y = (long long) a[k + j + i] * w[k] % mod;
+				reduce(a[k + j] += y - mod), reduce(a[k + j + i] = x - y);
+			}
+	}
+	if (!typ) {
+        reverse(a.begin() + 1, a.begin() + lim);
+		for (int i = 0; i < lim; ++i) a[i] = (long long) a[i] * inv_lim % mod;
+	}
+}
+vector<int> multiply(vector<int> &f, vector<int> &g) {
+    int n=(int)f.size() + (int)g.size() - 1;
+    precompute(n);
+    vector<int> a = f, b = g;
+    a.resize(lim); b.resize(lim);
+    ntt(a, 1), ntt(b, 1);
+    for (int i = 0; i < lim; ++i) a[i] = (long long) a[i] * b[i] % mod;
+    ntt(a, 0);
+    while((int)a.size() && a.back() == 0) a.pop_back();
+    return a;
+}
+int fact[N], ifact[N];
+vector<int> shift(vector<int> &f, int c) { //f(x + c)
+    int n=(int)f.size();
+    precompute(n + n - 1);
+    vector<int> a = f; a.resize(lim);
+    for (int i = 0; i < n; ++i) a[i] = (long long) a[i] * fact[i] % mod;
+    reverse(a.begin(), a.begin()+n);
+    vector<int> b; b.resize(lim); b[0] = 1;
+    for (int i = 1; i < n; ++i) b[i] = (long long) b[i - 1] * c % mod;
+    for (int i = 0; i < n; ++i) b[i] = (long long) b[i] * ifact[i] % mod;
+    ntt(a, 1), ntt(b, 1);
+    for (int i = 0; i < lim; ++i) a[i] = (long long) a[i] * b[i] % mod;
+    ntt(a, 0), reverse(a.begin(), a.begin() + n);
+    vector<int> g; g.resize(n);
+    for (int i = 0; i < n; ++i) g[i] = (long long) a[i] * ifact[i] % mod;
+    return g;
+}
+vector<int> range_mul(int n) { //(x+1)*(x+2)*(x+3)...(x+n)
+	if (n == 0) return vector<int>({1});
+	if (n & 1) {
+		vector<int> f = range_mul(n - 1);
+		f.push_back(0);
+		for (int i = (int)f.size()-1; i; --i) f[i] = (f[i - 1] + (long long) n * f[i]) % mod;
+		f[0] = (long long) f[0] * n % mod;
+		return f;
+	} 
+    else {
+		int n_ = n >> 1;
+		vector<int> f = range_mul(n_);
+		vector<int> tmp = shift(f, n_);
+		return multiply(f, tmp);
+	}
+}
+int stirling(int n, int k) {
+    if(n == 0 && k == 0) return 1;
+    if(n <= 0 || k <= 0) return 0;
+    vector<int> x = vector<int>({0, 1});
+    vector<int> y = range_mul(n - 1);
+    vector<int> ans = multiply(x, y);
+    if(k >= (int)ans.size()) return 0;
+    return ans[k];
+}
+int ncr(int n, int r) {
+    if(r < 0 || n < r) return 0;
+    return 1LL * fact[n] * ifact[r] % mod * ifact[n - r] % mod;
+}
+int main()
+{
+    fact[0] = 1;
+	for (int i = 1; i < N; ++i) fact[i] = (long long) fact[i - 1] * i % mod;
+	ifact[N - 1] = POW(fact[N - 1], mod - 2);
+	for (int i = N - 1; i; --i) ifact[i - 1] = (long long) ifact[i] * i % mod;
+    int n, a, b; cin >> n >> a >> b;
+    cout << 1LL * stirling(n - 1, a + b - 2) * ncr(a + b - 2, a - 1) % mod << '\n';
+    return 0;
+}
+//https://codeforces.com/problemset/problem/960/G

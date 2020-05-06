7f0a317bb4f0aeda3710fd9effc3714f9c26ee3d
ShahjalalShohag <33003206+ShahjalalShohag@users.noreply.github.com>
2020-05-06 12:45:40 +0600

    Update Number of Partitions.cpp

--- Math/Number of Partitions.cpp
+++ Math/Number of Partitions.cpp
@@ -42,7 +42,7 @@ using mint = modint<mod>;
 // O(nsqrt(n))
 vector<mint> Gen(int n) {
     vector<mint> ans(n + 1);
-    vector<pair<int, int>> gp;
+    vector<pair<int, int>> gp; //(sign, generalized pentagonal numbers)
     gp.emplace_back(0, 0);
     for(int i = 1; gp.back().second <= n; i++) {
         gp.emplace_back(i % 2 ? 1 : -1, i * (3*i - 1) / 2);
@@ -50,7 +50,10 @@ vector<mint> Gen(int n) {
     }
     ans[1] = 1;
     for(int i = 2; i <= n; i++) {
-        for(auto it : gp) if(i >= it.second) ans[i] += ans[i - it.second] * it.first;
+        for(auto it : gp) {
+            if(i >= it.second) ans[i] += ans[i - it.second] * it.first;
+            else break;
+        }
     }
     return ans;
 }

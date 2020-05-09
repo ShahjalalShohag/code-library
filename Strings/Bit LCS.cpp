12fc29ab87083581352e8746b2da34c984a4de10
ShahjalalShohag <33003206+ShahjalalShohag@users.noreply.github.com>
2020-05-09 20:02:53 +0600

    Update Bit LCS.cpp

--- Strings/Bit LCS.cpp
+++ Strings/Bit LCS.cpp
@@ -1,3 +1,7 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+
 #include <bits/stdc++.h>
 using namespace std;
 /* Bit-String Longest Common Subsequence Algorithm

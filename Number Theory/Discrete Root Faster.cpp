d05768f941308e81cbeb3003c54fa6b20b4cb848
ShahjalalShohag <33003206+ShahjalalShohag@users.noreply.github.com>
2020-06-09 17:39:09 +0600

    Add files via upload

---
+++ Number Theory/Discrete Root Faster.cpp
@@ -0,0 +1,101 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 3e5 + 9;
+
+long long power(long long a, long long n, long long p) {
+    long long r = 1;
+    for (; n > 0; n >>= 1,a = a * a % p) if(n % 2 == 1) r = r * a % p;
+    return r;
+}
+int cnt(long long a, long long base,long long p) {
+    int ret = 0;
+    while (a != 1) {
+        a = power(a, base, p);
+        ++ret;
+    }
+    return ret;
+}
+long long inverse(long long a, long long p) {
+    a %= p;
+    long long u = 1, v = 0;
+    long long b = p;
+    while (b > 0) {
+        long long q = a / b;
+        a %= b;
+        u -= v * q % p;
+        u = (u % p + p) % p;
+        u ^= v; v ^= u;
+        u ^= v; a ^= b;
+        b ^= a; a ^= b;
+    }
+    return u < 0 ? u + p : u;
+}
+long long gcd(long long a,long long b) {
+    return a == 0 ? b : gcd(b % a, a);
+}
+long long peth_root(long long a, long long p, int e, long long mod) {
+    long long q = mod - 1;
+    int s = 0;
+    while (q % p == 0) {
+        q /= p;
+        ++s;
+    }
+    long long pe = power(p, e, mod);
+    long long ans = power(a, ((pe - 1) * inverse(q, pe) % pe * q + 1) / pe, mod);
+    long long c = 2;
+    while (power(c, (mod - 1)/p, mod) == 1) ++c;
+    c = power(c, q, mod);
+    map<long long, int> mp;
+    long long add = 1;
+    int v = (int)sqrt((double)(s - e) * p) + 1;
+    long long mul = power(c, v * power(p, s - 1, mod - 1) % (mod - 1), mod);
+    for (int i = 0; i <= v; ++i) {
+        mp[add] = i;
+        add = add * mul % mod;
+    }
+    mul = inverse(power(c, power(p, s - 1, mod - 1), mod), mod);
+    for (int i = e; i<s; ++i) {
+        long long err = inverse(power(ans, pe, mod), mod) * a % mod;
+        long long target = power(err, power(p, s - 1 - i, mod - 1), mod);
+        for (int j = 0; j <= v; ++j) {
+            if (mp.find(target) != mp.end()) {
+                int x = mp[target];
+                ans = ans * power(c, (j + v * x) * power(p, i - e, mod - 1) % (mod - 1), mod) % mod;
+                break;
+            }
+            target = target * mul % mod;
+            assert(j != v);
+        }
+    }
+    return ans;
+}
+// Find any x such that x ^ k  =  a (mod p),  p is a prime
+// Complexity: O(p ^ (1 / 4))
+long long DiscreteRoot(long long a, long long k, long long p) {
+    if (k > 0 && a == 0) return 0;
+    k %= p - 1;
+    long long g = gcd(k, p - 1);
+    if (power(a, (p - 1) / g, p) != 1) return  - 1;
+    a = power(a, inverse(k / g, (p - 1) / g), p);
+    for (long long div = 2; div * div <= g; ++div) {
+        int sz = 0;
+        while (g % div == 0) g /= div, ++sz;
+        if (sz > 0) {
+            long long b = peth_root(a, div, sz, p);
+            a = b;
+        }
+    }
+    if (g > 1) a = peth_root(a, g, 1, p);
+    return a;
+}
+int32_t main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+    int t; cin >> t;
+    while (t--) {
+        long long k, a, p; cin >> k >> a >> p;
+        cout << DiscreteRoot(a, k, p) << '\n';
+    }
+}
+// https://judge.yosupo.jp/problem/kth_root_mod

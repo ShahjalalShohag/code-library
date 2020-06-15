e8e7b15957607facaa679933fb743e725d91e339
ShahjalalShohag <33003206+ShahjalalShohag@users.noreply.github.com>
2020-06-16 04:15:45 +0600

    Update Suffix Automaton.cpp

--- Strings/Suffix Automaton.cpp
+++ Strings/Suffix Automaton.cpp
@@ -1,25 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define pii pair<int,int>
-#define pll pair<ll,ll>
-#define eb emplace_back
-#define ll long long
-#define nl '\n'
-#define deb(x) cerr<<#x" = "<<x<<nl
-#define in() ( { int a ; scanf("%d",&a); a; } )
-
 const int N = 3e5 + 9;
-const int mod = 1e9 + 7;
 
-/**
-len -> largest string length of the corresponding endpos-equivalent class
-link -> longest suffix that is another endpos-equivalent class.
-firstpos -> 1 indexed end position of the first occurrence of the largest string of that node
-minlen(v) -> smallest string of node v = len(link(v)) + 1
-terminal nodes -> store the suffixes
-**/
-struct SuffixAutomaton{
+// len -> largest string length of the corresponding endpos-equivalent class
+// link -> longest suffix that is another endpos-equivalent class.
+// firstpos -> 1 indexed end position of the first occurrence of the largest string of that node
+// minlen(v) -> smallest string of node v = len(link(v)) + 1
+// terminal nodes -> store the suffixes
+struct SuffixAutomaton {
     struct node {
         int len, link, firstpos;
         map<char, int> nxt;
@@ -27,42 +16,35 @@ struct SuffixAutomaton{
     int sz, last;
     vector<node> t;
     vector<int> terminal;
-    vector<ll> dp;
-    SuffixAutomaton(){}
-    void init(int n)
-    {
-        t.clear();
-        terminal.clear();
-        dp.clear();
-        t.resize(2*n);
-        terminal.resize(2*n);
-        dp.resize(2*n, -1);
-        sz = 1; last = 0;
-        t[0].len = 0;
-        t[0].link = -1;
-        t[0].firstpos = 0;
+    vector<long long> dp;
+    SuffixAutomaton() {}
+    SuffixAutomaton(int n) {
+        t.resize(2 * n); terminal.resize(2 * n, 0);
+        dp.resize(2 * n, -1); sz = 1; last = 0;
+        t[0].len = 0; t[0].link = -1; t[0].firstpos = 0;
     }
-    void extend(char c)
-    {
+    void extend(char c) {
+        if (t[last].nxt.count(c) && t[t[last].nxt[c]].len == t[last].len + 1) {
+            last = t[last].nxt[c];
+            return;
+        }
         int cur = sz++;
         t[cur].len = t[last].len + 1;
         t[cur].firstpos = t[cur].len;
         int p = last;
-        while(p != -1 && !t[p].nxt.count(c)){
+        while (p != -1 && !t[p].nxt.count(c)) {
             t[p].nxt[c] = cur;
             p = t[p].link;
         }
-        if(p == -1) t[cur].link = 0;
-        else{
+        if (p == -1) t[cur].link = 0;
+        else {
             int q = t[p].nxt[c];
-            if(t[p].len + 1 == t[q].len) t[cur].link = q;
-            else{
+            if (t[p].len + 1 == t[q].len) t[cur].link = q;
+            else {
                 int clone = sz++;
-                t[clone].len = t[p].len+1;
-                t[clone].nxt = t[q].nxt;
-                t[clone].link = t[q].link;
-                t[clone].firstpos = t[q].firstpos;
-                while(p != -1 && t[p].nxt[c] == q){
+                t[clone] = t[q];
+                t[clone].len = t[p].len + 1;
+                while (p != -1 && t[p].nxt[c] == q) {
                     t[p].nxt[c] = clone;
                     p = t[p].link;
                 }
@@ -71,31 +53,33 @@ struct SuffixAutomaton{
         }
         last = cur;
     }
-    void get_terminals()
-    {
-        int p = last;
-        while(p > 0) terminal[p] = 1, p = t[p].link;
+    void build(string &s) {
+        for (auto x: s) {
+            extend(x);
+            terminal[last] = 1;
+        }
     }
-    void build(string &s)
-    {
-        for (auto x: s) extend(x);
-        get_terminals();
-    }
-    ll cnt(int i)///number of times i-th node occurs in the string
-    {
-        if(dp[i]!=-1) return dp[i];
-        ll ret = terminal[i];
-        for(auto x: t[i].nxt) ret += cnt(x.second);
+    long long cnt(int i) { //number of times i-th node occurs in the string
+        if (dp[i] != -1) return dp[i];
+        long long ret = terminal[i];
+        for (auto &x: t[i].nxt) ret += cnt(x.second);
         return dp[i] = ret;
     }
-}sa;
+};
 
-int32_t main()
-{
-    char ch[N]; scanf("%s", ch); string s = ch;
-    int n = s.size(); sa.init(n); sa.build(s);
-    ll ans = 0; ///number of unique substrings
-    for(int i = 1; i < sa.sz; i++) ans += sa.t[i].len - sa.t[sa.t[i].link].len;
-    cout << ans <<nl;
+int32_t main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t; cin >> t;
+    while (t--) {
+        string s; cin >> s;
+        int n = s.size();
+        SuffixAutomaton sa(n);
+        sa.build(s);
+        long long ans = 0; //number of unique substrings
+        for (int i = 1; i < sa.sz; i++) ans += sa.t[i].len - sa.t[sa.t[i].link].len;
+        cout << ans << '\n';
+    }
     return 0;
 }
+

3570061b7d269f7779b95ef957155f82fb5728f2
ShahjalalShohag <33003206+ShahjalalShohag@users.noreply.github.com>
2020-06-20 00:22:33 +0600

    Add files via upload

---
+++ Data Structures/Trie.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 3e5 + 9;
+
+struct Trie {
+	static const int B = 31;
+    struct node {
+        node* nxt[2]; int sz;
+        node() { nxt[0] = nxt[1] = NULL; sz = 0; }
+    }*root;
+    Trie() { root = new node(); }
+    void insert(int val) {
+        node* cur = root; cur -> sz++;
+        for (int i = B - 1; i >= 0; i--) {
+            int b = val >> i & 1;
+            if (cur -> nxt[b] == NULL) cur -> nxt[b] = new node();
+            cur = cur -> nxt[b]; cur -> sz++;
+        }
+    }
+    int query(int x, int k) { // number of values s.t. val ^ x < k
+        node* cur = root; int ans = 0;
+        for (int i = B - 1; i >= 0; i--) {
+            if (cur == NULL) break;
+            int b1 = x >> i & 1, b2 = k >> i & 1;
+            if (b2 == 1) {
+                if (cur -> nxt[b1]) ans += cur -> nxt[b1] -> sz;
+                cur = cur -> nxt[!b1];
+            }
+            else cur = cur -> nxt[b1];
+        }
+        return ans;
+    }
+    int get_max(int x) { // returns maximum of val ^ x
+        node* cur = root; int ans = 0;
+        for (int i = B - 1; i >= 0; i--) {
+            int k = x >> i & 1;
+            if (cur -> nxt[!k]) cur = cur -> nxt[!k], ans <<= 1, ans++;
+            else cur = cur -> nxt[k], ans <<= 1;
+        }
+        return ans;
+    }
+    int get_min(int x) { // returns minimum of val ^ x
+        node* cur = root; int ans = 0;
+        for (int i = B - 1; i >= 0; i--) {
+            int k = x >> i & 1;
+            if (cur -> nxt[k]) cur = cur -> nxt[k], ans <<= 1;
+            else cur = cur -> nxt[!k], ans <<= 1, ans++;
+        }
+        return ans;
+    }
+    void del(node* cur) {
+        for (int i = 0; i < 2; i++) if (cur -> nxt[i]) del(cur -> nxt[i]);
+        delete(cur);
+    }
+}t;
+int32_t main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n, k; cin >> n >> k;
+    int cur = 0; long long ans = 1LL * n * (n + 1) / 2;
+    t.insert(cur);
+    for (int i = 0; i < n; i++) {
+    	int x; cin >> x; cur ^= x;
+    	ans -= t.query(cur, k);
+    	t.insert(cur);
+    }
+    cout << ans << '\n';
+    return 0;
+}

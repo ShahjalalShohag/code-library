86420ba518160518286eadf00d793b71a3581c94
ShahjalalShohag <33003206+ShahjalalShohag@users.noreply.github.com>
2020-12-17 02:13:38 +0600

    Update Square Root Decomposition With Reverse Update.cpp

--- Data Structures/Square Root Decomposition With Reverse Update.cpp
+++ Data Structures/Square Root Decomposition With Reverse Update.cpp
@@ -1,101 +1,117 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+  
 const int N = 1e5 + 9, B = 330;
 
-int a[N];
-int t[4000][4000], z[4000];
-int blocks[3500], bid;
-int L[N], R[N], T, n, assign[N], rev[N];
-int create_block(int l, int r, int as) {
-  ++T;
-  z[T] = 0;
-  L[T] = l; R[T] = r;
-  if (!as) {
-    for (int i = l; i <= r; i++) {
-      t[T][z[T]++] = a[i];
+struct Block {
+  vector<int> v, sorted_v; int sz = 0;
+  int assigned = -1;
+  bool rev = false;
+  Block() {}
+  Block(int s, int to_assign, bool to_rev = false) {
+    sz = s; rev = to_rev;
+    assigned = to_assign;
+  }
+  Block(vector<int> &a, int to_assign = -1, bool to_rev = false) {
+    v = a; assigned = to_assign;
+    sz = a.size();
+    rev = to_rev;
+    sorted_v = v;
+    sort(sorted_v.begin(), sorted_v.end());
+  }
+  int get(int x) {
+    if (assigned != -1) {
+      return assigned >= x ? sz : 0;
     }
-    sort(t[T], t[T] + z[T]);
-    assign[T] = 0;
+    return sorted_v.size() - (lower_bound(sorted_v.begin(), sorted_v.end(), x) - sorted_v.begin());
   }
-  else {
-    assign[T] = as;
+  int size() {
+    return sz;
   }
-  rev[T] = 0;
-  return T;
-}
-void rebuild() {
-  T = 0; bid = 0;
+  void perform_reverse() {
+    rev ^= true;
+    if (assigned != -1) return;
+    reverse(v.begin(), v.end());
+  }
+  void truncate(int k) {
+    sz = k;
+    if (rev) {
+      perform_reverse();
+    }
+    if (assigned != -1) return;
+    v.resize(k);
+    sorted_v = v;
+    sort(sorted_v.begin(), sorted_v.end());
+  }
+}t[2000];
+vector<int> a, blocks;
+int n, T;
+inline void rebuild() {
+  T = 0; blocks.clear();
   for (int i = 1; i <= n; i += B) {
     int l = i, r = min(i + B - 1, n);
-    blocks[bid++] = create_block(l, r, 0);
+    vector<int> cur(a.begin() + l, a.begin() + r + 1);
+    t[++T] = Block(cur);
+    blocks.push_back(T);
   }
 }
-int nxt[3500], nid, fresh[5], fid;
-void update_blocks(int l, int r) {
+// returns such 𝑗(0 indexed), that the 𝑖-the element is the beginning of the 𝑗-th segment
+int split(int i) {
+  if (i > n) return blocks.size();
   int cur = 1;
-  nid = 0;
-  for (int w = 0; w < bid; w++) {
-    int id = blocks[w];
-    int cl = cur, cr = cur + R[id] - L[id];
-    if (cl >= l && cr <= r) {
-      nxt[nid++] = id;
-    }
-    else {
-      int le = max(cl, l), ri = min(cr, r);
-      if (le <= ri) {
-        int realL = L[id] + le - cl;
-        int realR = L[id] + ri - cl;
-        if (rev[id]) {
-          realR = R[id] - (le - cl);
-          realL = R[id] - (ri - cl);
-        }
-        fid = 0;
-        if (rev[id]) {
-          if (realR < R[id]) {
-            fresh[fid++] = create_block(realR + 1, R[id], assign[id]);
-          }
-          fresh[fid++] = create_block(realL, realR, assign[id]);
-          if (realL > L[id]) {
-            fresh[fid++] = create_block(L[id], realL - 1, assign[id]);
-          }
-        }
-        else {
-          if (realL > L[id]) {
-            fresh[fid++] = create_block(L[id], realL - 1, assign[id]);
-          }
-          fresh[fid++] = create_block(realL, realR, assign[id]);
-          if (realR < R[id]) {
-            fresh[fid++] = create_block(realR + 1, R[id], assign[id]);
-          }
-        }
-        for (int w = 0; w < fid; w++) {
-          int x = fresh[w];
-          nxt[nid++] = x;
-          rev[x] = rev[id];
-          assign[x] = assign[id];
-        }
+  for (auto it = blocks.begin(); it != blocks.end(); ++it) {
+    int id = *it;
+    int cl = cur, cr = cur + t[id].size() - 1;
+    if (cl <= i && i <= cr) {
+      if (i == cl) {
+        return it - blocks.begin();
       }
       else {
-        nxt[nid++] = id;
+        if (t[id].rev) {
+          t[id].perform_reverse();
+        }
+        int k = i - cl;
+        int cur = ++T;
+        if (t[id].assigned != -1) {
+          t[cur] = Block(cr - cl + 1 - k, t[id].assigned);
+        }
+        else {
+          vector<int> tmp = vector<int> (t[id].v.begin() + k, t[id].v.end());
+          t[cur] = Block(tmp);
+        }
+        t[id].truncate(k);
+        ++it;
+        it = blocks.insert(it, cur);
+        return it - blocks.begin();
       }
     }
     cur = cr + 1;
   }
-  for (int i = 0; i < nid; i++) {
-    blocks[i] = nxt[i];
-  }
-  bid = nid;
+  return 0;
+}
+// insert x into the i-th index
+void insert(int i, int x) {
+  a[n++] = x;
+  int j = split(i);
+  int cur = ++T; vector<int> b({x});
+  t[cur] = Block(b);
+  blocks.insert(blocks.begin() + j, cur);
+}
+// erase the i-th element
+void erase(int i) {
+  int j = split(i);
+  split(i + 1);
+  blocks.erase(blocks.begin() + j);
 }
 int ty[N], l[N], r[N], x[N];
 int32_t main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
   cin >> n;
-  vector<int> v;
+  a.resize(n + 1);
   for (int i = 1; i <= n; i++) {
     cin >> a[i];
-    v.push_back(a[i]);
+    a[i]++;
   }
   int q; cin >> q;
   for (int i = 1; i <= q; i++) {
@@ -103,88 +119,62 @@ int32_t main() {
     if (p == "get") {
       ty[i] = 3;
       cin >> l[i] >> r[i] >> x[i];
-      v.push_back(x[i]);
+      x[i]++;
     }
     else if (p == "set") {
       ty[i] = 1;
       cin >> l[i] >> r[i] >> x[i];
-      v.push_back(x[i]);
+      x[i]++;
     }
     else {
       ty[i] = 2;
       cin >> l[i] >> r[i];
     }
   }
-  sort(v.begin(), v.end());
-  v.erase(unique(v.begin(), v.end()), v.end());
-  for (int i = 1; i <= n; i++) {
-    a[i] = upper_bound(v.begin(), v.end(), a[i]) - v.begin();
-  }
+  rebuild();
   for (int i = 1; i <= q; i++) {
-    if (ty[i] != 2) {
-      x[i] = upper_bound(v.begin(), v.end(), x[i]) - v.begin();
-    }
-  }
-  vector<int> b(n + 1);
-  for (int i = 1; i <= n; i++) {
-    b[i] = a[i];
-  }
-  for (int i = 1; i <= q; i++) {
-    if ((i - 1) % 600 == 0) {
+    if (i % 600 == 0) {
       int cur = 1;
-      for (int w = 0; w < bid; w++) {
-        int id = blocks[w];
-        int cl = cur, cr = cur + R[id] - L[id];
-        for (int i = cl; i <= cr; i++) {
-          int rid = rev[id] ? R[id] - (i - cl) : L[id] + (i - cl);
-          b[i] = assign[id] ? assign[id] : a[rid];
+      a.resize(1);
+      for (auto x: blocks) {
+        if (t[x].rev) {
+          t[x].perform_reverse();
         }
-        cur = cr + 1;
-      }
-      for (int i = 1; i <= n; i++) {
-        a[i] = b[i];
+        if (t[x].assigned == -1) {
+          for (auto y: t[x].v) {
+            a.push_back(y);
+          }
+        }
+        else {
+          for (int j = 0; j < t[x].sz; j++) {
+            a.push_back(t[x].assigned);
+          }
+        }
       }
       rebuild();
     }
-    update_blocks(l[i], r[i]);
-    if (ty[i] == 1) {
-      int cur = 1;
-      for (int w = 0; w < bid; w++) {
-        int id = blocks[w];
-        int cl = cur, cr = cur + R[id] - L[id];
-        if (cl >= l[i] && cr <= r[i]) {
-          assign[id] = x[i];
-        }
-        cur = cr + 1;
+    if (ty[i] == 1) { // for all a_i: l <= i <= r, set a_i = x
+      int p = split(l[i]), q = split(r[i] + 1);
+      while (p < q) {
+        t[blocks[p]].assigned = x[i];
+        ++p;
       }
     }
-    else if (ty[i] == 3) {
-      int cur = 1, ans = 0;
-      for (int w = 0; w < bid; w++) {
-        int id = blocks[w];
-        int cl = cur, cr = cur + R[id] - L[id];
-        if (cl >= l[i] && cr <= r[i]) {
-          if (assign[id]) {
-            ans += (R[id] - L[id] + 1) * (assign[id] >= x[i]);
-          }
-          else ans += z[id] - (lower_bound(t[id], t[id] + z[id], x[i]) - t[id]);
-        }
-        cur = cr + 1;
+    else if (ty[i] == 3) { // find the number of elements in [l, r] s.t. a_i >= x
+      int ans = 0;
+      int p = split(l[i]), q = split(r[i] + 1);
+      while (p < q) {
+        ans += t[blocks[p]].get(x[i]);
+        ++p;
       }
       cout << ans << '\n';
     }
-    else {
-      int cur = 1, st = N, en = 0;
-      for (int j = 0; j < bid; j++) {
-        int cl = cur, cr = cur + R[blocks[j]] - L[blocks[j]];
-        if (cl >= l[i] && cr <= r[i]) {
-          st = min(st, j);
-          en = j;
-          rev[blocks[j]] ^= 1;
-        }
-        cur = cr + 1;
+    else { // reverse a subarray
+      int p = split(l[i]), q = split(r[i] + 1);
+      for (int j = p; j < q; j++) {
+        t[blocks[j]].rev ^= true;
       }
-      reverse(blocks + st, blocks + en + 1);
+      reverse(blocks.begin() + p, blocks.begin() + q);
     }
   }
   return 0;

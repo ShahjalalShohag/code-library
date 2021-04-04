c1e10cc72ba1e85915e851020ac3aece4b801743
ShahjalalShohag <33003206+ShahjalalShohag@users.noreply.github.com>
2021-04-04 22:56:44 +0600

    Update Kirchoffs Theorem.cpp

--- Graph Theory/Kirchoffs Theorem.cpp
+++ Graph Theory/Kirchoffs Theorem.cpp
@@ -1,56 +1,61 @@
 #include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
 using namespace std;
 
-const int N = 105;
+const int N = 205, mod = 1000210433;
 
-int pow(long long n, long long k, const int mod)
-{
-    int ans = 1 % mod; n %= mod;
-    while(k > 0){
-        if(k & 1) ans = 1LL * ans * n % mod;
-        n = 1LL * n * n % mod;
+int power(long long n, long long k) {
+    int ans = 1;
+    while (k) {
+        if (k & 1) ans = (long long) ans * n % mod;
+        n = (long long) n * n % mod;
         k >>= 1;
     }
     return ans;
 }
-
-int det(vector<vector<long long>>a, const int mod)
-{
-    int n=a.size();
-    for(int i = 0; i < n; i++) for(int j = 0; j < n; j++) a[i][j] = (a[i][j] + mod) % mod;
-    int det = 1;
-    for (int i=0; i<n; ++i){
-        int k = i;
-        for (int j=i+1; j<n; ++j)  if (a[j][i] > a[k][i]) k = j;
-        if(a[k][i] == 0) return 0;
-        swap (a[i], a[k]);
-        if (i != k) det = mod - det;
-        det = a[i][i] * det % mod;
-        int d = pow(a[i][i], mod - 2, mod);
-        for (int j = i+1; j<n; ++j) a[i][j]  = a[i][j] * d % mod;
-        for (int j=0; j<n; ++j) if (j != i && a[j][i]){
-                for (int k=i+1; k<n; ++k)
-                    a[j][k] -= a[i][k] * a[j][i] % mod, a[j][k] = (a[j][k] + mod) % mod;
+int det(vector<vector<int>> a){
+    int n = a.size(), m = (int)a[0].size();
+    int free_var = 0;
+    const long long MODSQ = (long long)mod * mod;
+    int det = 1, rank = 0;
+    for (int col = 0, row = 0; col < m && row < n; col++) {
+        int mx = row;
+        for (int k = row; k < n; k++) if (a[k][col] > a[mx][col]) mx = k;
+        if (a[mx][col] == 0) {det = 0; continue;}
+        for (int j = col; j < m; j++) swap(a[mx][j], a[row][j]);
+      if (row != mx) det = det == 0 ? 0 : mod - det;
+      det = 1LL * det * a[row][col] % mod;
+        int inv = power(a[row][col], mod - 2);
+        for (int i = 0; i < n && inv; i++){
+            if (i != row && a[i][col]) {
+                int x = ((long long)a[i][col] * inv) % mod;
+                for (int j = col; j < m && x; j++){
+                    if (a[row][j]) a[i][j] = (MODSQ + a[i][j] - ((long long)a[row][j] * x)) % mod;
+                }
+            }
         }
+        row++; ++rank;
     }
     return det;
 }
 struct dsu {
-	vector<int> par, rnk, size; int c;
-	dsu(int n) : par(n+1), rnk(n+1,0), size(n+1,1), c(n) {
-		for (int i = 1; i <= n; ++i) par[i] = i;
-	}
-	int find(int i) { return (par[i] == i ? i : (par[i] = find(par[i]))); }
-	bool same(int i, int j) { return find(i) == find(j); }
-	int get_size(int i) { return size[find(i)]; }
-	int count() { return c; } //connected components
-	int merge(int i, int j) {
-		if ((i = find(i)) == (j = find(j))) return -1; else --c;
-		if (rnk[i] > rnk[j]) swap(i, j);
-		par[i] = j; size[j] += size[i];
-		if (rnk[i] == rnk[j]) rnk[j]++;
-		return j;
-	}
+  vector<int> par, rnk, size; int c;
+  dsu(int n) : par(n+1), rnk(n+1,0), size(n+1,1), c(n) {
+    for (int i = 1; i <= n; ++i) par[i] = i;
+  }
+  int find(int i) { return (par[i] == i ? i : (par[i] = find(par[i]))); }
+  bool same(int i, int j) { return find(i) == find(j); }
+  int get_size(int i) { return size[find(i)]; }
+  int count() { return c; } //connected components
+  int merge(int i, int j) {
+    if ((i = find(i)) == (j = find(j))) return -1; else --c;
+    if (rnk[i] > rnk[j]) swap(i, j);
+    par[i] = j; size[j] += size[i];
+    if (rnk[i] == rnk[j]) rnk[j]++;
+    return j;
+  }
 };
 int w[N][N], deg[N], vis[N];
 vector<int> g[N];
@@ -60,15 +65,19 @@ inline void add2(int u, int v)
     w[u][v]++;
     deg[u]++;
 }
+int f;
 inline void add(int u, int v)
 {
     if(u == v) return;
+    f = 1;
     g[u].push_back(v); g[v].push_back(u);
 }
-map<int, int> mp;
+gp_hash_table<int, int> mp;
 int get_id(int k)
 {
-    if(mp.find(k) == mp.end()) mp[k] = mp.size();
+    if(mp.find(k) == mp.end()) {
+      mp[k]; mp[k] = mp.size();
+    }
     return mp[k];
 }
 void dfs(int u)
@@ -79,69 +88,63 @@ void dfs(int u)
         add2(get_id(u), get_id(v));
     }
 }
-int extended_euclid(int a, int b, int &x, int &y) {
-    int xx = y = 0;
-    int yy = x = 1;
-    while (b) {
-        int q = a / b;
-        int t = b; b = a%b; a = t;
-        t = xx; xx = x - q*xx; x = t;
-        t = yy; yy = y - q*yy; y = t;
-    }
-    return a;
-}
-
-pair<int, int> CRT(int x, int a, int y, int b) {
-	int s, t;
-	int d = extended_euclid(x, y, s, t);
-	if (a % d != b % d) return make_pair(0, -1);
-	int m=x*y;
-	s%=m;
-	t%=m;
-	s=(s+m)%m;
-	t=(t+m)%m;
-	return make_pair( (s * b%m * x%m + t * a%m * y%m)% m/d, m/ d);
-}
+// number of maximum spanning trees
 int32_t main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-
-    int n, m; cin >> n >> m;
-    vector< array<int, 3> > ed;
-    for(int i = 1; i <= m; i++){
-        int u, v, w; cin >> u >> v >> w;
-        ed.push_back({w, u, v});
-    }
-    sort(ed.begin(), ed.end());
-    dsu d(n);
-    int ans1 = 1, ans2 = 1;
-    for(int i = 0, j; i < m; i = j){
-        memset(vis, 0, sizeof vis);
-        for(int i = 1; i <= n; i++) g[i].clear();
-        add(d.find(ed[i][1]), d.find(ed[i][2]));
-        for(j = i + 1; j < m && ed[i][0] == ed[j][0]; j++) add(d.find(ed[j][1]), d.find(ed[j][2]));
-        for(int u = 1; u <= n; u++) if(!vis[u]){
-            memset(w, 0, sizeof w);
-            memset(deg, 0, sizeof deg);
-            dfs(u);
-            int sz = mp.size();
-            mp.clear();
-            if(sz <= 1) continue;
-            for(int u = 1; u <= sz; u++) for(int v = 1; v <= sz; v++){
-                if(u == v) w[u][v] = deg[u];
-                else w[u][v] *= -1;
+    int t, cs = 0; cin >> t;
+    while (t--) {
+      int n, m; cin >> n >> m;
+      vector< array<int, 3> > ed;
+      dsu d(n);
+      for(int i = 1; i <= m; i++){
+          int u, v, w; cin >> u >> v >> w;
+          d.merge(u, v);
+          ed.push_back({w, u, v});
+      }
+      if (d.count() != 1) {
+        cout << "Case " << ++cs << ": " << 0 << '\n';
+        continue;
+      }
+      sort(ed.rbegin(), ed.rend());
+      d = dsu(n);
+      int ans = 1;
+      for(int i = 0, j; i < m; i = j){
+          memset(vis, 0, sizeof vis);
+          for(int i = 1; i <= n; i++) g[i].clear();
+          f = 0;
+          add(d.find(ed[i][1]), d.find(ed[i][2]));
+          for(j = i + 1; j < m && ed[i][0] == ed[j][0]; j++) add(d.find(ed[j][1]), d.find(ed[j][2]));
+          if (!f) continue;
+          memset(w, 0, sizeof w);
+          memset(deg, 0, sizeof deg);
+          for(int u = 1; u <= n; u++) if(!vis[u]){
+              mp.clear();
+              dfs(u);
+              int sz = mp.size();
+              if(sz <= 1) {
+                w[1][1] = 0; deg[1] = 0;
+                continue;
+              }
+              for(int u = 1; u <= sz; u++) for(int v = 1; v <= sz; v++){
+                  if(u == v) w[u][v] = deg[u];
+                  else w[u][v] = (mod - w[u][v]) % mod;
+              }
+              vector< vector<int> > mat(sz - 1, vector<int> (sz - 1, 0));
+              for(int i = 1; i < sz; i++) for(int j = 1; j < sz; j++) mat[i - 1][j - 1] = w[i][j];
+              ans = 1LL * ans * det(mat) % mod;
+            for (int i = 1; i <= sz; i++) {
+              for (int j = 1; j <= sz; j++) {
+                w[i][j] = 0;
+              }
+              deg[i] = 0;
             }
-            vector< vector<long long> > mat(sz - 1, vector<long long> (sz - 1, 0));
-            for(int i = 1; i < sz; i++) for(int j = 1; j < sz; j++) mat[i - 1][j - 1] = w[i][j];
-            ans1 = ans1 * det(mat, 3) % 3;
-            ans2 = ans2 * det(mat, 10337) % 10337;
-        }
-        for(int k = i; k < j; k++) d.merge(ed[k][1], ed[k][2]);
+          }
+          for(int k = i; k < j; k++) d.merge(ed[k][1], ed[k][2]);
+      }
+      cout << "Case " << ++cs << ": " << ans << '\n';
     }
-    cout << CRT(3, ans1, 10337, ans2).first << '\n';
     return 0;
 }
-/*
-https://www.spoj.com/problems/MSTS/
-*/
+// https://lightoj.com/contest/loj-homecoming/arena/problem/13

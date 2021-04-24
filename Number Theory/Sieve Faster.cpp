fb61a751e5dffbcb6e2af66209caeb6ff2dc1440
ShahjalalShohag <33003206+ShahjalalShohag@users.noreply.github.com>
2021-04-25 02:17:45 +0600

    Update Sieve Faster.cpp

--- Number Theory/Sieve Faster.cpp
+++ Number Theory/Sieve Faster.cpp
@@ -1,99 +1,92 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-const int N = 3e5 + 9;
-
-// Credit: Min_25
+// credit: min_25
+// takes 0.5s for n = 1e9
 vector<int> sieve(const int N, const int Q = 17, const int L = 1 << 15) {
-    static const int rs[] = {1, 7, 11, 13, 17, 19, 23, 29};
-    struct P {
-        P(int p) : p(p) {}
-        int p;
-        int pos[8];
-    };
+  static const int rs[] = {1, 7, 11, 13, 17, 19, 23, 29};
+  struct P { 
+    P(int p) : p(p) {}
+    int p; int pos[8];
+  };
+  auto approx_prime_count = [] (const int N) -> int {
+    return N > 60184 ? N / (log(N) - 1.1)
+                     : max(1., N / (log(N) - 1.11)) + 1;
+  };
 
-    auto approx_prime_count = [] (const int N) -> int {
-        return N > 60184 ? N / (log(N) - 1.1)
-        : max(1., N / (log(N) - 1.11)) + 1;
-    };
+  const int v = sqrt(N), vv = sqrt(v);
+  vector<bool> isp(v + 1, true);
+  for (int i = 2; i <= vv; ++i) if (isp[i]) {
+    for (int j = i * i; j <= v; j += i) isp[j] = false;
+  }
 
-    const int v = sqrt(N), vv = sqrt(v);
-    vector<bool> isp(v + 1, true);
-    for (int i = 2; i <= vv; ++i) if (isp[i]) {
-            for (int j = i * i; j <= v; j += i) isp[j] = false;
-        }
+  const int rsize = approx_prime_count(N + 30);
+  vector<int> primes = {2, 3, 5}; int psize = 3;
+  primes.resize(rsize);
 
-    const int rsize = approx_prime_count(N + 30);
-    vector<int> primes = {2, 3, 5};
-    int psize = 3;
-    primes.resize(rsize);
-
-    vector<P> sprimes;
-    size_t pbeg = 0;
-    int prod = 1;
-    for (int p = 7; p <= v; ++p) {
-        if (!isp[p]) continue;
-        if (p <= Q) prod *= p, ++pbeg, primes[psize++] = p;
-        auto pp = P(p);
-        for (int t = 0; t < 8; ++t) {
-            int j = (p <= Q) ? p : p * p;
-            while (j % 30 != rs[t]) j += p << 1;
-            pp.pos[t] = j / 30;
-        }
-        sprimes.push_back(pp);
+  vector<P> sprimes; size_t pbeg = 0;
+  int prod = 1; 
+  for (int p = 7; p <= v; ++p) {
+    if (!isp[p]) continue;
+    if (p <= Q) prod *= p, ++pbeg, primes[psize++] = p;
+    auto pp = P(p); 
+    for (int t = 0; t < 8; ++t) {
+      int j = (p <= Q) ? p : p * p;
+      while (j % 30 != rs[t]) j += p << 1;
+      pp.pos[t] = j / 30;
     }
+    sprimes.push_back(pp);
+  }
 
-    vector<unsigned char> pre(prod, 0xFF);
-    for (size_t pi = 0; pi < pbeg; ++pi) {
-        auto pp = sprimes[pi];
-        const int p = pp.p;
-        for (int t = 0; t < 8; ++t) {
-            const unsigned char m = ~(1 << t);
-            for (int i = pp.pos[t]; i < prod; i += p) pre[i] &= m;
-        }
+  vector<unsigned char> pre(prod, 0xFF);
+  for (size_t pi = 0; pi < pbeg; ++pi) {
+    auto pp = sprimes[pi]; const int p = pp.p;
+    for (int t = 0; t < 8; ++t) {
+      const unsigned char m = ~(1 << t);
+      for (int i = pp.pos[t]; i < prod; i += p) pre[i] &= m;
     }
+  }
 
-    const int block_size = (L + prod - 1) / prod * prod;
-    vector<unsigned char> block(block_size);
-    unsigned char* pblock = block.data();
-    const int M = (N + 29) / 30;
+  const int block_size = (L + prod - 1) / prod * prod;
+  vector<unsigned char> block(block_size); unsigned char* pblock = block.data();
+  const int M = (N + 29) / 30;
 
-    for (int beg = 0; beg < M; beg += block_size, pblock -= block_size) {
-        int end = min(M, beg + block_size);
-        for (int i = beg; i < end; i += prod) {
-            copy(pre.begin(), pre.end(), pblock + i);
-        }
-        if (beg == 0) pblock[0] &= 0xFE;
-        for (size_t pi = pbeg; pi < sprimes.size(); ++pi) {
-            auto& pp = sprimes[pi];
-            const int p = pp.p;
-            for (int t = 0; t < 8; ++t) {
-                int i = pp.pos[t];
-                const unsigned char m = ~(1 << t);
-                for (; i < end; i += p) pblock[i] &= m;
-                pp.pos[t] = i;
-            }
-        }
-        for (int i = beg; i < end; ++i) {
-            for (int m = pblock[i]; m > 0; m &= m - 1) {
-                primes[psize++] = i * 30 + rs[__builtin_ctz(m)];
-            }
-        }
+  for (int beg = 0; beg < M; beg += block_size, pblock -= block_size) {
+    int end = min(M, beg + block_size);
+    for (int i = beg; i < end; i += prod) {
+      copy(pre.begin(), pre.end(), pblock + i);
     }
-    assert(psize <= rsize);
-    while (psize > 0 && primes[psize - 1] > N) --psize;
-    primes.resize(psize);
-    return primes;
+    if (beg == 0) pblock[0] &= 0xFE;
+    for (size_t pi = pbeg; pi < sprimes.size(); ++pi) {
+      auto& pp = sprimes[pi];
+      const int p = pp.p;
+      for (int t = 0; t < 8; ++t) {
+        int i = pp.pos[t]; const unsigned char m = ~(1 << t);
+        for (; i < end; i += p) pblock[i] &= m;
+        pp.pos[t] = i;
+      }
+    }
+    for (int i = beg; i < end; ++i) {
+      for (int m = pblock[i]; m > 0; m &= m - 1) {
+        primes[psize++] = i * 30 + rs[__builtin_ctz(m)];
+      }
+    }
+  }
+  assert(psize <= rsize);
+  while (psize > 0 && primes[psize - 1] > N) --psize;
+  primes.resize(psize);
+  return primes;
 }
+
 int32_t main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	int n, a, b; cin >> n >> a >> b;
-	auto primes = sieve(n);
-	vector<int> ans;
-	for (int i = b; i < primes.size() && primes[i] <= n; i += a) ans.push_back(primes[i]);
-	cout << primes.size() << ' ' << ans.size() << '\n';
-	for (auto x: ans) cout << x << ' '; cout << '\n';
-    return 0;
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  int n, a, b; cin >> n >> a >> b;
+  auto primes = sieve(n);
+  vector<int> ans;
+  for (int i = b; i < primes.size() && primes[i] <= n; i += a) ans.push_back(primes[i]);
+  cout << primes.size() << ' ' << ans.size() << '\n';
+  for (auto x: ans) cout << x << ' '; cout << '\n';
+  return 0;
 }
 // https://judge.yosupo.jp/problem/enumerate_primes

de241bd363cc7989916fa9e7ab39387831b32cf8
ShahjalalShohag <33003206+ShahjalalShohag@users.noreply.github.com>
2021-08-03 23:16:34 +0600

    Update and rename Lagrange Interpolation Polynomial Extraction.cpp to Lagrange Interpolation with Polynomial Extraction.cpp

---
+++ Math/Lagrange Interpolation with Polynomial Extraction.cpp
@@ -0,0 +1,216 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 1e6 + 9, mod = 1e9 + 7;
+
+struct base {
+  double x, y;
+  base() { x = y = 0; }
+  base(double x, double y): x(x), y(y) { }
+};
+inline base operator + (base a, base b) { return base(a.x + b.x, a.y + b.y); }
+inline base operator - (base a, base b) { return base(a.x - b.x, a.y - b.y); }
+inline base operator * (base a, base b) { return base(a.x * b.x - a.y * b.y, a.x * b.y + a.y * b.x); }
+inline base conj(base a) { return base(a.x, -a.y); }
+int lim = 1;
+vector<base> roots = {{0, 0}, {1, 0}};
+vector<int> rev = {0, 1};
+const double PI = acosl(- 1.0);
+void ensure_base(int p) {
+  if(p <= lim) return;
+  rev.resize(1 << p);
+  for(int i = 0; i < (1 << p); i++) rev[i] = (rev[i >> 1] >> 1) + ((i & 1)  <<  (p - 1));
+  roots.resize(1 << p);
+  while(lim < p) {
+    double angle = 2 * PI / (1 << (lim + 1));
+    for(int i = 1 << (lim - 1); i < (1 << lim); i++) {
+      roots[i << 1] = roots[i];
+      double angle_i = angle * (2 * i + 1 - (1 << lim));
+      roots[(i << 1) + 1] = base(cos(angle_i), sin(angle_i));
+    }
+    lim++;
+  }
+}
+void fft(vector<base> &a, int n = -1) {
+  if(n == -1) n = a.size();
+  assert((n & (n - 1)) == 0);
+  int zeros = __builtin_ctz(n);
+  ensure_base(zeros);
+  int shift = lim - zeros;
+  for(int i = 0; i < n; i++) if(i < (rev[i] >> shift)) swap(a[i], a[rev[i] >> shift]);
+  for(int k = 1; k < n; k <<= 1) {
+    for(int i = 0; i < n; i += 2 * k) {
+      for(int j = 0; j < k; j++) {
+        base z = a[i + j + k] * roots[j + k];
+        a[i + j + k] = a[i + j] - z;
+        a[i + j] = a[i + j] + z;
+      }
+    }
+  }
+}
+//eq = 0: 4 FFTs in total
+//eq = 1: 3 FFTs in total
+vector<int> multiply(vector<int> &a, vector<int> &b, int eq = 0) {
+  int need = a.size() + b.size() - 1;
+  int p = 0;
+  while((1 << p) < need) p++;
+  ensure_base(p);
+  int sz = 1 << p;
+  vector<base> A, B;
+  if(sz > (int)A.size()) A.resize(sz);
+  for(int i = 0; i < (int)a.size(); i++) {
+    int x = (a[i] % mod + mod) % mod;
+    A[i] = base(x & ((1 << 15) - 1), x >> 15);
+  }
+  fill(A.begin() + a.size(), A.begin() + sz, base{0, 0});
+  fft(A, sz);
+  if(sz > (int)B.size()) B.resize(sz);
+  if(eq) copy(A.begin(), A.begin() + sz, B.begin());
+  else {
+    for(int i = 0; i < (int)b.size(); i++) {
+      int x = (b[i] % mod + mod) % mod;
+      B[i] = base(x & ((1 << 15) - 1), x >> 15);
+    }
+    fill(B.begin() + b.size(), B.begin() + sz, base{0, 0});
+    fft(B, sz);
+  }
+  double ratio = 0.25 / sz;
+  base r2(0,  - 1), r3(ratio, 0), r4(0,  - ratio), r5(0, 1);
+  for(int i = 0; i <= (sz >> 1); i++) {
+    int j = (sz - i) & (sz - 1);
+    base a1 = (A[i] + conj(A[j])), a2 = (A[i] - conj(A[j])) * r2;
+    base b1 = (B[i] + conj(B[j])) * r3, b2 = (B[i] - conj(B[j])) * r4;
+    if(i != j) {
+      base c1 = (A[j] + conj(A[i])), c2 = (A[j] - conj(A[i])) * r2;
+      base d1 = (B[j] + conj(B[i])) * r3, d2 = (B[j] - conj(B[i])) * r4;
+      A[i] = c1 * d1 + c2 * d2 * r5;
+      B[i] = c1 * d2 + c2 * d1;
+    }
+    A[j] = a1 * b1 + a2 * b2 * r5;
+    B[j] = a1 * b2 + a2 * b1;
+  }
+  fft(A, sz); fft(B, sz);
+  vector<int> res(need);
+  for(int i = 0; i < need; i++) {
+    long long aa = A[i].x + 0.5;
+    long long bb = B[i].x + 0.5;
+    long long cc = A[i].y + 0.5;
+    res[i] = (aa + ((bb % mod) << 15) + ((cc % mod) << 30))%mod;
+  }
+  return res;
+}
+
+template <int32_t MOD>
+struct modint {
+  int32_t value;
+  modint() = default;
+  modint(int32_t value_) : value(value_) {}
+  inline modint<MOD> operator + (modint<MOD> other) const { int32_t c = this->value + other.value; return modint<MOD>(c >= MOD ? c - MOD : c); }
+  inline modint<MOD> operator - (modint<MOD> other) const { int32_t c = this->value - other.value; return modint<MOD>(c <    0 ? c + MOD : c); }
+  inline modint<MOD> operator * (modint<MOD> other) const { int32_t c = (int64_t)this->value * other.value % MOD; return modint<MOD>(c < 0 ? c + MOD : c); }
+  inline modint<MOD> & operator += (modint<MOD> other) { this->value += other.value; if (this->value >= MOD) this->value -= MOD; return *this; }
+  inline modint<MOD> & operator -= (modint<MOD> other) { this->value -= other.value; if (this->value <    0) this->value += MOD; return *this; }
+  inline modint<MOD> & operator *= (modint<MOD> other) { this->value = (int64_t)this->value * other.value % MOD; if (this->value < 0) this->value += MOD; return *this; }
+  inline modint<MOD> operator - () const { return modint<MOD>(this->value ? MOD - this->value : 0); }
+  modint<MOD> pow(uint64_t k) const {
+    modint<MOD> x = *this, y = 1;
+    for (; k; k >>= 1) {
+      if (k & 1) y *= x;
+      x *= x;
+    }
+    return y;
+  }
+  modint<MOD> inv() const { return pow(MOD - 2); }  // MOD must be a prime
+  inline modint<MOD> operator /  (modint<MOD> other) const { return *this *  other.inv(); }
+  inline modint<MOD> operator /= (modint<MOD> other)       { return *this *= other.inv(); }
+  inline bool operator == (modint<MOD> other) const { return value == other.value; }
+  inline bool operator != (modint<MOD> other) const { return value != other.value; }
+  inline bool operator < (modint<MOD> other) const { return value < other.value; }
+  inline bool operator > (modint<MOD> other) const { return value > other.value; }
+};
+template <int32_t MOD> modint<MOD> operator * (int64_t value, modint<MOD> n) { return modint<MOD>(value) * n; }
+template <int32_t MOD> modint<MOD> operator * (int32_t value, modint<MOD> n) { return modint<MOD>(value % MOD) * n; }
+template <int32_t MOD> ostream & operator << (ostream & out, modint<MOD> n) { return out << n.value; }
+
+using mint = modint<mod>;
+
+struct combi{
+  int n; vector<mint> facts, finvs, invs;
+  combi(int _n): n(_n), facts(_n), finvs(_n), invs(_n){
+    facts[0] = finvs[0] = 1;
+    invs[1] = 1;
+    for (int i = 2; i < n; i++) invs[i] =  invs[mod % i] * (-mod / i);
+    for(int i = 1; i < n; i++){
+      facts[i] = facts[i - 1] * i;
+      finvs[i] = finvs[i - 1] * invs[i];
+    }
+  }
+  inline mint fact(int n) { return facts[n]; }
+  inline mint finv(int n) { return finvs[n]; }
+  inline mint inv(int n) { return invs[n]; }
+  inline mint ncr(int n, int k) { return facts[n] * finvs[k] * finvs[n-k]; }
+};
+combi C(N);
+
+vector<mint> multiply_mint(vector<mint> &a, vector<mint> &b) {
+  vector<int> A, B;
+  for(auto x: a) A.push_back(x.value);
+  for(auto x: b) B.push_back(x.value);
+  auto res = multiply(A, B, (A == B));
+  vector<mint> ans;
+  for(auto x: res) ans.push_back(mint(x));
+  while ((int)ans.size() && ans.back() == 0) ans.pop_back();
+  return ans;
+}
+vector<mint> add(vector<mint> a, vector<mint> b) {
+  int n = max(a.size(), b.size());
+  vector<mint> ans(n);
+  for(int i = 0; i < n; i++) ans[i] = (i < a.size() ? a[i] : 0) + (i < b.size() ? b[i] : 0);
+  while ((int)ans.size() && ans.back() == 0) ans.pop_back();
+  return ans;
+}
+vector<mint> t[N * 4];
+void build(int id, int l, int r) {
+  if (l == r) {
+    t[id] = {-l, 1};
+    return;
+  }
+  int mid = l + r >> 1;
+  build(id << 1, l, mid);
+  build(id << 1 | 1, mid + 1, r);
+  t[id] = multiply_mint(t[id << 1], t[id << 1 | 1]);
+}
+vector<mint> yo(int id, int l, int r, vector<mint> &p) {
+  if (l == r) {
+    int n = (int)p.size() - 1;
+    mint tmp = ((n - l) & 1 ? -1 : 1);
+    return {p[l] * tmp * C.finv(l) * C.finv(n - l)};
+  }
+  int mid = l + r >> 1;
+  vector<mint> L = yo(id << 1, l, mid, p);
+  vector<mint> R = yo(id << 1 | 1, mid + 1, r, p);
+  return add(multiply_mint(L, t[id << 1 | 1]), multiply_mint(t[id << 1], R));
+}
+//takes 0.8sec for n = 100000
+//Extracts the polynomial explicitely
+vector<mint> Lagrange(vector<mint> &p) {
+  int n = p.size();
+  build(1, 0, n - 1);
+  auto ans = yo(1, 0, n - 1, p); ans.resize(n);
+  return ans;
+}
+int32_t main() {
+  int n, k; cin >> n >> k;
+  vector<mint> p;
+  mint sum = 0; p.push_back(0);
+  for (int i = 1; i <= k + 1; i++) {
+    sum += mint(i).pow(k);
+    p.push_back(sum);
+  }
+  auto a = Lagrange(p);
+  mint ans = 0;
+  for (int i = 0; i < a.size(); i++) ans += a[i] * mint(n).pow(i);
+  cout << ans << '\n';
+  return 0;
+}
+//https://codeforces.com/problemset/problem/622/F

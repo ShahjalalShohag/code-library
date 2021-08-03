ada604277b7a8cc280de117f02e309f22fab94a9
ShahjalalShohag <33003206+ShahjalalShohag@users.noreply.github.com>
2021-08-04 01:44:44 +0600

    Update Determinant Modular.cpp

--- Math/Determinant Modular.cpp
+++ Math/Determinant Modular.cpp
@@ -1,48 +1,47 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-const int N = 105, mod = 1e9 + 7;
+const int N = 105, mod = 998244353;
 
 int power(long long n, long long k) {
-    int ans = 1 % mod; n %= mod; if (n < 0) n += mod;
-    while (k) {
-        if (k & 1) ans = (long long) ans * n % mod;
-        n = (long long) n * n % mod;
-        k >>= 1;
-    }
-    return ans;
+  int ans = 1 % mod; n %= mod; if (n < 0) n += mod;
+  while (k) {
+    if (k & 1) ans = (long long) ans * n % mod;
+    n = (long long) n * n % mod;
+    k >>= 1;
+  }
+  return ans;
 }
-int Gauss(vector<vector<int>> a){
-    int n = a.size(), m = (int)a[0].size();
-    int free_var = 0;
-    const long long MODSQ = (long long)mod * mod;
-    int det = 1, rank = 0;
-    for (int col = 0, row = 0; col < m && row < n; col++) {
-        int mx = row;
-        for (int k = row; k < n; k++) if (a[k][col] > a[mx][col]) mx = k;
-        if (a[mx][col] == 0) {det = 0; continue;}
-        for (int j = col; j < m; j++) swap(a[mx][j], a[row][j]);
-    	if (row != mx) det = det == 0 ? 0 : mod - det;
-    	det = 1LL * det * a[row][col] % mod;
-        int inv = power(a[row][col], mod - 2);
-        for (int i = 0; i < n && inv; i++){
-            if (i != row && a[i][col]) {
-                int x = ((long long)a[i][col] * inv) % mod;
-                for (int j = col; j < m && x; j++){
-                    if (a[row][j]) a[i][j] = (MODSQ + a[i][j] - ((long long)a[row][j] * x)) % mod;
-                }
-            }
+int Gauss(vector<vector<int>> a) {
+  int n = a.size(), m = (int)a[0].size();
+  int free_var = 0;
+  const long long MODSQ = (long long)mod * mod;
+  int det = 1, rank = 0;
+  for (int col = 0, row = 0; col < m && row < n; col++) {
+    int mx = row;
+    for (int k = row; k < n; k++) if (a[k][col] > a[mx][col]) mx = k;
+    if (a[mx][col] == 0) {det = 0; continue;}
+    for (int j = col; j < m; j++) swap(a[mx][j], a[row][j]);
+    if (row != mx) det = det == 0 ? 0 : mod - det;
+    det = 1LL * det * a[row][col] % mod;
+    int inv = power(a[row][col], mod - 2);
+    for (int i = 0; i < n && inv; i++){
+      if (i != row && a[i][col]) {
+        int x = ((long long)a[i][col] * inv) % mod;
+        for (int j = col; j < m && x; j++){
+          if (a[row][j]) a[i][j] = (MODSQ + a[i][j] - ((long long)a[row][j] * x)) % mod;
         }
-        row++; ++rank;
+      }
     }
-    return det;
+    row++; ++rank;
+  }
+  return det;
 }
 
-int32_t main()
-{
-    int n, m; cin >> n >> m;
-    vector<vector<int>> a(n, vector<int>(m));
-    for(int i = 0; i < n; i++) for(int j = 0; j < m; j++) cin >> a[i][j];
-   	cout << Gauss(a) << '\n';
-    return 0;
+int32_t main() {
+  int n; cin >> n;
+  vector<vector<int>> a(n, vector<int>(n));
+  for(int i = 0; i < n; i++) for(int j = 0; j < n; j++) cin >> a[i][j];
+  cout << Gauss(a) << '\n';
+  return 0;
 }

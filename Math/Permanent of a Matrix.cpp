5a8c8bb469a03b47c8cb3ad3cb2e909d7dadbaca
ShahjalalShohag <33003206+ShahjalalShohag@users.noreply.github.com>
2021-08-04 14:02:33 +0600

    Update Permanent of a Matrix.cpp

--- Math/Permanent of a Matrix.cpp
+++ Math/Permanent of a Matrix.cpp
@@ -1,39 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-const int N = 1030;
+const int N = 20;
 
-int a[N][N], p[N], n, vis[N];
-void yo(int cur) {
-	if (vis[cur]) return;
-	vis[cur] = 1;
-	for (int i = 1; i <= n; i++) {
-		for (int j = i + 1; j <= n; j++) {
-			int nxt = cur ^ a[i][p[i]] ^ a[j][p[j]] ^ a[i][p[j]] ^ a[j][p[i]];
-			swap(p[i], p[j]);
-			yo(nxt);
-			swap(p[i], p[j]);
-		}
-	}
-}
+long long dp[1 << N];
+int n, a[N][N];
 int32_t main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    int t; cin >> t;
-    while (t--) {
-    	cin >> n;
-    	int cur = 0;
-    	for (int i = 1; i <= n; i++) {
-    		for (int j = 1; j <= n; j++) {
-    			cin >> a[i][j];
-    			if (i == j) p[i] = i, cur ^= a[i][i];
-    		}
-    	}
-    	memset(vis, 0, sizeof vis);
-    	yo(cur);
-    	for (int i = 0; i < N; i++) if (vis[i]) cout << i << ' ';
-    	cout << '\n';
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  int t; cin >> t;
+  while (t--) {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) cin >> a[i][j];
     }
-    return 0;
+    memset(dp, 0, sizeof dp);
+    dp[(1 << n) - 1] = 1;
+    for (int mask = (1 << n) - 2; mask >= 0; mask--) {
+      int i = __builtin_popcount(mask);
+      for (int j = 0; j < n; j++) if (!(mask >> j & 1)) dp[mask] += dp[mask | 1 << j] * a[i][j];
+    }
+    cout << dp[0] << '\n'; // permanent of the matrix
+  }
+  return 0;
 }
-//https://www.codechef.com/problems/CHEFLST

8df901c6ccd63107533ae828b449830022260e59
ShahjalalShohag <33003206+ShahjalalShohag@users.noreply.github.com>
2021-08-05 12:41:57 +0600

    Add files via upload

---
+++ Graph Theory/LCA in O(1).cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+template <class T>
+struct RMQ { // 0-based
+  vector<vector<T>> rmq;
+  T kInf = numeric_limits<T>::max();
+  void build(const vector<T>& V) {
+    int n = V.size(), on = 1, dep = 1;
+    while (on < n) on *= 2, ++dep;
+    rmq.assign(dep, V);
+ 
+    for (int i = 0; i < dep - 1; ++i)
+      for (int j = 0; j < n; ++j) {
+        rmq[i + 1][j] = min(rmq[i][j], rmq[i][min(n - 1, j + (1 << i))]);
+      }
+  }
+  T query(int a, int b) { // [a, b)
+    if (b <= a) return kInf;
+    int dep = 31 - __builtin_clz(b - a); // log(b - a)
+    return min(rmq[dep][a], rmq[dep][b - (1 << dep)]);
+  }
+};
+
+struct LCA { // 0-based
+  vector<int> enter, depth, exxit;
+  vector<vector<int>> G;
+  vector<pair<int, int>> linear;
+  RMQ<pair<int, int>> rmq;
+  int timer = 0;
+  LCA() {} 
+  LCA(int n) : enter(n, -1), exxit(n, -1), depth(n), G(n), linear(2 * n) {}
+  void dfs(int node, int dep) {
+    linear[timer] = {dep, node};
+    enter[node] = timer++;
+    depth[node] = dep;
+    for (auto vec : G[node])
+    if (enter[vec] == -1) {
+      dfs(vec, dep + 1);
+      linear[timer++] = {dep, node};
+    }
+    exxit[node] = timer;
+  }
+  void add_edge(int a, int b) {
+    G[a].push_back(b);
+    G[b].push_back(a);
+  }
+  void build(int root) {
+    dfs(root, 0);
+    rmq.build(linear);
+  }
+  int query(int a, int b) {
+    a = enter[a], b = enter[b];
+    return rmq.query(min(a, b), max(a, b) + 1).second;
+  }
+  int dist(int a, int b) {
+    return depth[a] + depth[b] - 2 * depth[query(a, b)];
+  }
+};
+ 
+LCA lca;
+
+int32_t main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  
+  return 0;
+}

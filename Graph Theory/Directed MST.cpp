c7de3b55ce26fb585c061ea1ccd7828bef8887fc
ShahjalalShohag <33003206+ShahjalalShohag@users.noreply.github.com>
2021-08-05 16:00:31 +0600

    Update Directed MST.cpp

--- Graph Theory/Directed MST.cpp
+++ Graph Theory/Directed MST.cpp
@@ -6,103 +6,103 @@ const int N = 3e5 + 9;
 const long long inf = 1e18;
 
 template<typename T> struct PQ {
-    long long sum = 0;
-    priority_queue<T, vector<T>, greater<T>> Q;
-    void push(T x) { x.w -= sum; Q.push(x); }
-    T pop() { auto ans = Q.top(); Q.pop(); ans.w += sum; return ans; }
-    int size() { return Q.size(); }
-    void add(long long x) { sum += x; }
-    void merge(PQ &&x) {
-        if (size() < x.size()) swap(sum, x.sum), swap(Q, x.Q);
-        while (x.size()) {
-            auto tmp = x.pop();
-            tmp.w -= sum;
-            Q.push(tmp);
-        }
+  long long sum = 0;
+  priority_queue<T, vector<T>, greater<T>> Q;
+  void push(T x) { x.w -= sum; Q.push(x); }
+  T pop() { auto ans = Q.top(); Q.pop(); ans.w += sum; return ans; }
+  int size() { return Q.size(); }
+  void add(long long x) { sum += x; }
+  void merge(PQ &&x) {
+    if (size() < x.size()) swap(sum, x.sum), swap(Q, x.Q);
+    while (x.size()) {
+      auto tmp = x.pop();
+      tmp.w -= sum;
+      Q.push(tmp);
     }
+  }
 };
 struct edge {
-    int u, v; long long w;
-    bool operator > (const edge &rhs) const { return w > rhs.w; }
+  int u, v; long long w;
+  bool operator > (const edge &rhs) const { return w > rhs.w; }
 };
 struct DSU {
-    vector<int> par;
-    DSU (int n) : par(n, -1) {}
-    int root(int i) { return par[i] < 0 ? i : par[i] = root(par[i]); }
-    void set_par(int c, int p) { par[c] = p; }
+  vector<int> par;
+  DSU (int n) : par(n, -1) {}
+  int root(int i) { return par[i] < 0 ? i : par[i] = root(par[i]); }
+  void set_par(int c, int p) { par[c] = p; }
 };
-// returns parentss of each vertex
+// returns parents of each vertex
 // each edge should be distinct
-// it assumes that a solution exists
+// it assumes that a solution exists (all vertices are reachable from root)
 // 0 indexed
 // Takes ~300ms for n = 2e5
 vector<int> DMST(int n, int root, const vector<edge> &edges) {
-    vector<int> u(2 * n - 1, -1), par(2 * n - 1, -1);
-    edge par_edge[2 * n - 1];
-    vector<int> child[2 * n - 1];
+  vector<int> u(2 * n - 1, -1), par(2 * n - 1, -1);
+  edge par_edge[2 * n - 1];
+  vector<int> child[2 * n - 1];
 
-    PQ<edge> Q[2 * n - 1];
-    for (auto e : edges) Q[e.v].push(e);
-    for (int i = 0; i < n; i++) Q[(i + 1) % n].push({i, (i + 1) % n, inf});
+  PQ<edge> Q[2 * n - 1];
+  for (auto e : edges) Q[e.v].push(e);
+  for (int i = 0; i < n; i++) Q[(i + 1) % n].push({i, (i + 1) % n, inf});
 
-    int super = n;
-    DSU dsu(2 * n - 1);
-    int head = 0;
-    while (Q[head].size()) {
-        auto x = Q[head].pop();
-        int nxt_root = dsu.root(x.u);
-        if (nxt_root == head) continue;
-        u[head] = nxt_root;
-        par_edge[head] = x;
-        if (u[nxt_root] == -1) head = nxt_root;
-        else {
-            int j = nxt_root;
-            do {
-                Q[j].add(-par_edge[j].w);
-                Q[super].merge(move(Q[j]));
-                assert(u[j] != -1);
-                child[super].push_back(j);
-                j = dsu.root(u[j]);
-            } while (j != nxt_root);
-            for (auto u : child[super]) par[u] = super, dsu.set_par(u, super);
-            head = super++;
-        }
+  int super = n;
+  DSU dsu(2 * n - 1);
+  int head = 0;
+  while (Q[head].size()) {
+    auto x = Q[head].pop();
+    int nxt_root = dsu.root(x.u);
+    if (nxt_root == head) continue;
+    u[head] = nxt_root;
+    par_edge[head] = x;
+    if (u[nxt_root] == -1) head = nxt_root;
+    else {
+      int j = nxt_root;
+      do {
+        Q[j].add(-par_edge[j].w);
+        Q[super].merge(move(Q[j]));
+        assert(u[j] != -1);
+        child[super].push_back(j);
+        j = dsu.root(u[j]);
+      } while (j != nxt_root);
+      for (auto u : child[super]) par[u] = super, dsu.set_par(u, super);
+      head = super++;
     }
-    vector<int> res(2 * n - 1, -1);
-    queue<int> q; q.push(root);
-    while (q.size()) {
-        int u = q.front();
-        q.pop();
-        while (par[u] != -1) {
-            for (auto v : child[par[u]]) {
-                if (v != u) {
-                    res[par_edge[v].v] = par_edge[v].u;
-                    q.push(par_edge[v].v);
-                    par[v] = -1;
-                }
-            }
-            u = par[u];
+  }
+  vector<int> res(2 * n - 1, -1);
+  queue<int> q; q.push(root);
+  while (q.size()) {
+    int u = q.front();
+    q.pop();
+    while (par[u] != -1) {
+      for (auto v : child[par[u]]) {
+        if (v != u) {
+          res[par_edge[v].v] = par_edge[v].u;
+          q.push(par_edge[v].v);
+          par[v] = -1;
         }
+      }
+      u = par[u];
     }
-    res[root] = root; res.resize(n);
-    return res;
+  }
+  res[root] = root; res.resize(n);
+  return res;
 }
 int32_t main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    int n, m, root; cin >> n >> m >> root;
-    vector<edge> edges(m);
-    for (auto &e : edges) cin >> e.u >> e.v >> e.w;
-    auto res = DMST(n, root, edges);
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  int n, m, root; cin >> n >> m >> root;
+  vector<edge> edges(m);
+  for (auto &e : edges) cin >> e.u >> e.v >> e.w;
+  auto res = DMST(n, root, edges);
 
-    unordered_map<int, int> W[n];
-    for (auto u : edges) W[u.v][u.u] = u.w;
+  unordered_map<int, int> W[n];
+  for (auto u : edges) W[u.v][u.u] = u.w;
 
-    long long ans = 0;
-    for (int i = 0; i < n; i++) if (i != root) ans += W[i][res[i]];
-    cout << ans << '\n';
-    for (auto x : res) cout << x << ' '; cout << '\n';
-    return 0;
+  long long ans = 0;
+  for (int i = 0; i < n; i++) if (i != root) ans += W[i][res[i]];
+  cout << ans << '\n';
+  for (auto x : res) cout << x << ' '; cout << '\n';
+  return 0;
 }
 // https://judge.yosupo.jp/problem/directedmst
 // http://www.cs.tau.ac.il/~zwick/grad-algo-13/directed-mst.pdf

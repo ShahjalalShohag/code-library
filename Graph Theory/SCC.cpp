fc63c31dd7a23a5612d8bc1b0f1444620c34086e
ShahjalalShohag <33003206+ShahjalalShohag@users.noreply.github.com>
2021-08-05 12:45:05 +0600

    Update SCC.cpp

--- Graph Theory/SCC.cpp
+++ Graph Theory/SCC.cpp
@@ -2,63 +2,62 @@
 using namespace std;
 const int N = 3e5 + 9;
 
-//given an undirected graph return the minimum number of edges to be added so that the whole graph  become an SCC
+// given a directed graph return the minimum number of edges to be added so that the whole graph  become an SCC
 bool vis[N];
 vector<int> g[N], r[N], G[N], vec; //G is the condensed graph
-void dfs1(int u)
-{
-    vis[u] = 1;
-    for(auto v: g[u]) if(!vis[v]) dfs1(v);
-    vec.push_back(u);
+void dfs1(int u) {
+  vis[u] = 1;
+  for(auto v: g[u]) if(!vis[v]) dfs1(v);
+  vec.push_back(u);
 }
 
 vector<int> comp;
-void dfs2(int u)
-{
-    comp.push_back(u);
-    vis[u] = 1;
-    for(auto v: r[u]) if(!vis[v]) dfs2(v);
+void dfs2(int u) {
+  comp.push_back(u);
+  vis[u] = 1;
+  for(auto v: r[u]) if(!vis[v]) dfs2(v);
 }
 
 int idx[N], in[N], out[N];
-int main()
-{
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
 
-    int n, m; cin >> n >> m;
-    for(int i = 1; i <= m; i++){
-        int u, v; cin >> u >> v;
-        g[u].push_back(v);
-        r[v].push_back(u);
+  int n, m;
+  cin >> n >> m;
+  for(int i = 1; i <= m; i++) {
+    int u, v;
+    cin >> u >> v;
+    g[u].push_back(v);
+    r[v].push_back(u);
+  }
+  for(int i = 1; i <= n; i++) if(!vis[i]) dfs1(i);
+  reverse(vec.begin(), vec.end());
+  memset(vis, 0, sizeof vis);
+  int scc = 0;
+  for(auto u: vec) {
+    if(!vis[u]) {
+      comp.clear();
+      dfs2(u);
+      scc++;
+      for(auto x: comp) idx[x]=scc;
     }
-    for(int i = 1; i <= n; i++) if(!vis[i]) dfs1(i);
-    reverse(vec.begin(), vec.end());
-    memset(vis, 0, sizeof vis);
-    int scc = 0;
-    for(auto u: vec){
-        if(!vis[u]){
-            comp.clear();
-            dfs2(u);
-            scc++;
-            for(auto x: comp) idx[x]=scc;
-        }
+  }
+  for(int u = 1; u <= n; u++) {
+    for(auto v: g[u]) {
+      if(idx[u] != idx[v]) {
+        in[idx[v]]++, out[idx[u]]++;
+        G[idx[u]].push_back(idx[v]);
+      }
     }
-    for(int u = 1; u <= n; u++){
-        for(auto v: g[u]){
-            if(idx[u] != idx[v]){
-                in[idx[v]]++, out[idx[u]]++;
-                G[idx[u]].push_back(idx[v]);
-            }
-        }
-    }
-    int needed_in=0, needed_out=0;
-    for(int i = 1; i <= scc; i++){
-        if(!in[i]) needed_in++;
-        if(!out[i]) needed_out++;
-    }
-    int ans = max(needed_in, needed_out);
-    if(scc == 1) ans = 0;
-    cout << ans << '\n';
-    return 0;
+  }
+  int needed_in=0, needed_out=0;
+  for(int i = 1; i <= scc; i++) {
+    if(!in[i]) needed_in++;
+    if(!out[i]) needed_out++;
+  }
+  int ans = max(needed_in, needed_out);
+  if(scc == 1) ans = 0;
+  cout << ans << '\n';
+  return 0;
 }

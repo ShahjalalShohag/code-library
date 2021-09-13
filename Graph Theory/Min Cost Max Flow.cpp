7982e2cc0d142ca5c448e0dc4cdaded39670ad5a
ShahjalalShohag <33003206+ShahjalalShohag@users.noreply.github.com>
2021-09-14 01:11:02 +0600

    Update Min Cost Max Flow.cpp

--- Graph Theory/Min Cost Max Flow.cpp
+++ Graph Theory/Min Cost Max Flow.cpp
@@ -1,101 +1,156 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-const int N = 6030;
+const int N = 3e5 + 9;
 
+
+//Works for both directed, undirected and with negative cost too
+//doesn't work for negative cycles
+//for undirected edges just make the directed flag false
+//Complexity: O(min(E^2 *V log V, E logV * flow))
+using T = long long;
+const T inf = 1LL << 61;
 struct MCMF {
-    using T = int; static const T inf = 1e9 + 7;
-    int n, m, s, t; bool vis[N];
-    int par[N], pos[N]; T pot[N], dis[N], left[N];
-    priority_queue<pair<T, int>> q;
-    struct edge {
-        int to, rev; T cap, cost, flow; int id;
-    };
-    vector<edge> ed[N];
-    MCMF() {}
-    MCMF(int _n) {
-        n = _n + 2;
-        for (int i = 1; i <= n; i++) ed[i].clear();
+  struct edge {
+    int u, v;
+    T cap, cost;
+    int id;
+    edge(int _u, int _v, T _cap, T _cost, int _id) {
+      u = _u;
+      v = _v;
+      cap = _cap;
+      cost = _cost;
+      id = _id;
     }
-    void add_edge(int u, int v, T cap, T cost, int id = -1) {
-        edge a = {v, (int)ed[v].size(), cap, cost, 0, id};
-        edge b = {u, (int)ed[u].size(), 0, -cost, 0, -1};
-        ed[u].push_back(a); ed[v].push_back(b);
-    }
-    T BellmanFord(int u) {
-        if (vis[u]) return pot[u];
-        if (u == s) {
-            pot[s] = 0;
-            return 0;
+  };
+  int n, s, t, mxid;
+  T flow, cost;
+  vector<vector<int>> g;
+  vector<edge> e;
+  vector<T> d, potential, flow_through;
+  vector<int> par;
+  bool neg;
+  MCMF() {}
+  MCMF(int _n) { // 0-based indexing
+    n = _n + 10;
+    g.assign(n, vector<int> ());
+    neg = false;
+    mxid = 0;
+  }
+  void add_edge(int u, int v, T cap, T cost, int id = -1, bool directed = true) {
+    if(cost < 0) neg = true;
+    g[u].push_back(e.size());
+    e.push_back(edge(u, v, cap, cost, id));
+    g[v].push_back(e.size());
+    e.push_back(edge(v, u, 0, -cost, -1));
+    mxid = max(mxid, id);
+    if(!directed) add_edge(v, u, cap, cost, -1, true);
+  }
+  bool dijkstra() {
+    par.assign(n, -1);
+    d.assign(n, inf);
+    priority_queue<pair<T, T>, vector<pair<T, T>>, greater<pair<T, T>> > q;
+    d[s] = 0;
+    q.push(pair<T, T>(0, s));
+    while (!q.empty()) {
+      int u = q.top().second;
+      T nw = q.top().first;
+      q.pop();
+      if(nw != d[u]) continue;
+      for (int i = 0; i < (int)g[u].size(); i++) {
+        int id = g[u][i];
+        int v = e[id].v;
+        T cap = e[id].cap;
+        T w = e[id].cost + potential[u] - potential[v];
+        if (d[u] + w < d[v] && cap > 0) {
+          d[v] = d[u] + w;
+          par[v] = id;
+          q.push(pair<T, T>(d[v], v));
         }
-        vis[u] = true; pot[u] = inf;
-        for (edge e : ed[u]) {
-            edge r = ed[e.to][e.rev];
-            if (r.flow < r.cap) pot[u] = min(pot[u], BellmanFord(e.to) + r.cost);
-        }
-        return pot[u];
+      }
     }
-    bool Dijkstra() {
-        memset(vis, 0, sizeof vis);
-        for (int i = 1; i <= n; i++) dis[i] = left[i] = inf;
-        dis[s] = 0; q.push({0, s}); int u, v;
-        while (!q.empty()) {
-            u = q.top().second; q.pop();
-            if (vis[u]) continue;
-            vis[u] = true; int ptr = 0;
-            for(edge e : ed[u]) {
-                v = e.to; T cost = e.cost + pot[u] - pot[v];
-                if (e.flow < e.cap && dis[u] + cost < dis[v]) {
-                    dis[v] = dis[u] + cost;
-                    par[v] = u; pos[v] = ptr;
-                    left[v] = min(left[u], e.cap - e.flow);
-                    q.push(make_pair(-dis[v], v));
-                }
-                ++ptr;
+    for (int i = 0; i < n; i++) { // update potential
+      if(d[i] < inf) potential[i] += d[i];
+    }
+    return d[t] != inf;
+  }
+  T send_flow(int v, T cur) {
+    if(par[v] == -1) return cur;
+    int id = par[v];
+    int u = e[id].u;
+    T w = e[id].cost;
+    T f = send_flow(u, min(cur, e[id].cap));
+    cost += f * w;
+    e[id].cap -= f;
+    e[id ^ 1].cap += f;
+    return f;
+  }
+  //returns {maxflow, mincost}
+  pair<T, T> solve(int _s, int _t, T goal = inf) {
+    s = _s;
+    t = _t;
+    flow = 0, cost = 0;
+    potential.assign(n, 0);
+    if (neg) {
+      // run Bellman-Ford to find starting potential
+      d.assign(n, inf);
+      for (int i = 0, relax = true; i < n && relax; i++) {
+        for (int u = 0; u < n; u++) {
+          for (int k = 0; k < (int)g[u].size(); k++) {
+            int id = g[u][k];
+            int v = e[id].v;
+            T cap = e[id].cap, w = e[id].cost;
+            if (d[v] > d[u] + w && cap > 0) {
+              d[v] = d[u] + w;
+              relax = true;
             }
+          }
         }
-        for (int i = 1; i <= n; i++) dis[i] += (pot[i] - pot[s]);
-        return vis[t];
+      }
+      for(int i = 0; i < n; i++) if(d[i] < inf) potential[i] = d[i];
     }
-    pair<T, T> solve(int _s, int _t, T goal = inf) {
-        s = _s; t = _t;
-        memset(pot, 0, sizeof pot);
-        memset(vis, 0, sizeof vis);
-        BellmanFord(t);
-        int u, v; T ans = 0, cost = 0, f;
-        while (ans < goal && Dijkstra()) {
-            u = t; f = left[t];
-            while (u != s) {
-                v = par[u];
-                ed[v][pos[u]].flow += left[t];
-                ed[u][ed[v][pos[u]].rev].flow -= left[t];
-                u = v;
-            }
-            T need = goal - ans; f = min(f, need);
-            ans += f; cost += f * dis[t];
-            memcpy(pot, dis, sizeof dis);
-        }
-        return make_pair(ans, cost);
+    while (flow < goal && dijkstra()) flow += send_flow(t, goal - flow);
+    flow_through.assign(mxid + 10, 0);
+    for (int u = 0; u < n; u++) {
+      for (auto v : g[u]) {
+        if (e[v].id >= 0) flow_through[e[v].id] = e[v ^ 1].cap;
+      }
     }
+    return make_pair(flow, cost);
+  }
 };
-int a[N];
-int32_t main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	int n; cin >> n;
-	for (int i = 1; i <= n; i++) cin >> a[i];
-	MCMF F(n * 2 + 4); int s = 2 * n + 1, t = s + 1;
-	for (int i = 1; i <= n; i++) {
-		for (int j = i + 1; j <= n; j++) {
-			if (abs(a[i] - a[j]) == 1 || abs(a[i] - a[j]) % 7 == 0) {
-				F.add_edge(i + n, j, 1, 0);
-			}
-		}
-		F.add_edge(s, i, 1, 0);
-		F.add_edge(i + n, t, 1, 0);
-		F.add_edge(i, i + n, 1, -1);
-	}
-	cout << -F.solve(s, t, 4).second << '\n';
-    return 0;
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  int n;
+  cin >> n;
+  assert(n <= 10);
+  MCMF F(2 * n);
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      int k;
+      cin >> k;
+      F.add_edge(i, j + n, 1, k, i * 20 + j);
+    }
+  }
+  int s = 2 * n + 1, t = s + 1;
+  for (int i = 0; i < n; i++) {
+    F.add_edge(s, i, 1, 0);
+    F.add_edge(i + n, t, 1, 0);
+  }
+  auto ans = F.solve(s, t).second;
+  long long w = 0;
+  set<int> se;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      int p = i * 20 + j;
+      if (F.flow_through[p] > 0) {
+        se.insert(j);
+        w += F.flow_through[p];
+      }
+    }
+  }
+  assert(se.size() == n && w == n);
+  cout << ans << '\n';
+  return 0;
 }
-//https://codeforces.com/contest/818/problem/G

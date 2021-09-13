4f8485edb7b636d9b2ec17be3ac7dd528ff18b67
ShahjalalShohag <33003206+ShahjalalShohag@users.noreply.github.com>
2021-09-14 01:48:34 +0600

    Update Stable Marriage Problem.cpp

--- Graph Theory/Stable Marriage Problem.cpp
+++ Graph Theory/Stable Marriage Problem.cpp
@@ -12,53 +12,55 @@ m[][],w[][] lists partners in order of their preferences
 it always yields the one that is best for all men among all stable matchings, and worst for all women. complexity O(n^2)*/
 
 int m[N][N], w[N][N], id[N], marry[N], ans[N], pref[N][N];
-void SM(int n)
-{
-    queue<int>q;
-    for(int i = 1; i <= n; i++){
-        q.push(i); id[i] = 1;
-    }
-    memset(marry, -1, sizeof marry);
-    for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= n; j++) pref[i][m[i][j]] = j;
-    }
-    while(!q.empty()){
-        int cw = q.front(); q.pop();
-        int cm = w[cw][id[cw]];
-        if(marry[cm] != -1){
-            if(pref[cm][marry[cm]] > pref[cm][cw]){
-                id[marry[cm]]++;
-                q.push(marry[cm]);
-                marry[cm]=cw;
-            }
-            else{
-                id[cw]++;
-                q.push(cw);
-            }
-        }
-        else marry[cm] = cw;
-    }
-    for(int i = 1; i <= n; i++) ans[i] = marry[i];
+void SM(int n) {
+  queue<int>q;
+  for(int i = 1; i <= n; i++) {
+    q.push(i);
+    id[i] = 1;
+  }
+  memset(marry, -1, sizeof marry);
+  for(int i = 1; i <= n; i++) {
+    for(int j = 1; j <= n; j++) pref[i][m[i][j]] = j;
+  }
+  while(!q.empty()) {
+    int cw = q.front();
+    q.pop();
+    int cm = w[cw][id[cw]];
+    if(marry[cm] != -1) {
+      if(pref[cm][marry[cm]] > pref[cm][cw]) {
+        id[marry[cm]]++;
+        q.push(marry[cm]);
+        marry[cm] = cw;
+      } else {
+        id[cw]++;
+        q.push(cw);
+      }
+    } else marry[cm] = cw;
+  }
+  for(int i = 1; i <= n; i++) ans[i] = marry[i];
 }
 
-int32_t main()
-{
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
+int32_t main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
 
-    int t; cin >> t;
-    while(t--){
-        int n; cin >> n;
-        for(int i = 1; i <= n; i++){
-            int k; cin >> k;
-            for(int j = 1; j <= n; j++) cin >> w[i][j];
-        }
-        for(int i = 1; i <= n; i++){
-            int k; cin >> k;
-            for(int j = 1; j <= n; j++) cin >> m[i][j];
-        }
-        SM(n);
-        for(int i = 1; i <= n; i++) cout << i << ' ' << ans[i] << '\n';
+  int t;
+  cin >> t;
+  while(t--) {
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; i++) {
+      int k;
+      cin >> k;
+      for(int j = 1; j <= n; j++) cin >> w[i][j];
     }
-    return 0;
+    for(int i = 1; i <= n; i++) {
+      int k;
+      cin >> k;
+      for(int j = 1; j <= n; j++) cin >> m[i][j];
+    }
+    SM(n);
+    for(int i = 1; i <= n; i++) cout << i << ' ' << ans[i] << '\n';
+  }
+  return 0;
 }

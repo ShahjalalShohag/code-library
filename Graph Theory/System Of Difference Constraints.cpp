436e69777e21c4e004b84ec26bdcb1db15e2f25b
ShahjalalShohag <33003206+ShahjalalShohag@users.noreply.github.com>
2021-09-13 18:22:07 +0600

    Update System Of Difference Constraints.cpp

--- Graph Theory/System Of Difference Constraints.cpp
+++ Graph Theory/System Of Difference Constraints.cpp
@@ -32,19 +32,18 @@ try to add negative edges for better complexity
 bool vis[N];
 int d[N];
 vector<pair<int, int>> g[N];
-bool spfa(int u)
-{
-	vis[u] = 1;
-	for(auto e: g[u]){
-	    int w = e.second, v = e.first;
-		if(d[u] + w > d[v]){ //d[u] + w < d[v] for T=0
-			if(vis[v]) return 0;
-			d[v] = d[u] + w;
-			if(!spfa(v)) return 0;
-		}
-	}
-	vis[u] = 0;
-	return 1;
+bool spfa(int u) {
+  vis[u] = 1;
+  for(auto e : g[u]) {
+    int w = e.second, v = e.first;
+    if(d[u] + w > d[v]) { //d[u] + w < d[v] for T=0
+      if(vis[v]) return 0;
+      d[v] = d[u] + w;
+      if(!spfa(v)) return 0;
+    }
+  }
+  vis[u] = 0;
+  return 1;
 }
 
 /*
@@ -53,45 +52,43 @@ here ty = 0 means OP is <=
 ty = 1 means OP is >=
 ty = 2 means OP is ==
 */
-void add(int u, int v, int w, int ty)
-{
-    if(ty == 1) add(v, u, -w, 0);
-    else if(ty == 2){
-        add(u, v, w, 0);
-        add(v, u, -w, 0);
-    }
-    else{
-        g[u].emplace_back(v, -w); //for T=1
-        //g[v].emplace_back(u, w); //for T=0
-    }
+void add(int u, int v, int w, int ty) {
+  if(ty == 1) add(v, u, -w, 0);
+  else if(ty == 2) {
+    add(u, v, w, 0);
+    add(v, u, -w, 0);
+  } else {
+    g[u].emplace_back(v, -w); //for T=1
+    //g[v].emplace_back(u, w); //for T=0
+  }
 }
 
 int a[N], ans[N];
-int32_t main()
-{
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
+int32_t main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
 
-    int n, l, r, k; cin >> n >> l >> r >> k;
-    for(int i = 1; i <= n; i++) cin >> a[i];
-    for(int i = 1; i < n; i++){
-        if(a[i] < a[i + 1]) add(i + 1, i, k, 0), add(i, i + 1, -1, 0);
-        else if(a[i] > a[i + 1]) add(i, i + 1, k, 0), add(i + 1, i, -1, 0);
-        else add(i + 1, i, 0, 0), add(i, i + 1, 0, 0);
+  int n, l, r, k;
+  cin >> n >> l >> r >> k;
+  for(int i = 1; i <= n; i++) cin >> a[i];
+  for(int i = 1; i < n; i++) {
+    if(a[i] < a[i + 1]) add(i + 1, i, k, 0), add(i, i + 1, -1, 0);
+    else if(a[i] > a[i + 1]) add(i, i + 1, k, 0), add(i + 1, i, -1, 0);
+    else add(i + 1, i, 0, 0), add(i, i + 1, 0, 0);
+  }
+  int s = n + 1;
+  for(int i = 1; i <= n; i++) add(i, s, r, 0), add(s, i, -l, 0);   //adding the constraint l <= a_i <= r
+  memset(vis, 0, sizeof vis);
+  for(int i = 1; i <= s ; i++) d[i] = -1e9 - 9; //1e9 + 9 for T = 0,  try to make INF as close to 0 as possible
+  d[s] = 0;
+  if(!spfa(s)) puts("-1");
+  else {
+    for(int i = 1; i <= n; i++) ans[i] = d[i] - d[s];
+    for(int i = 1; i <= n; i++) {
+      if(i > 1) cout << ' ';
+      cout << ans[i];
     }
-    int s = n + 1;
-    for(int i = 1; i <= n; i++) add(i, s , r, 0), add(s , i, -l, 0); //adding the constraint l <= a_i <= r
-    memset(vis, 0, sizeof vis);
-    for(int i = 1;i <= s ;i++) d[i] = -1e9 - 9; //1e9 + 9 for T = 0,  try to make INF as close to 0 as possible
-    d[s] = 0;
-    if(!spfa(s)) puts("-1");
-    else{
-        for(int i = 1; i <= n; i++) ans[i] = d[i] - d[s];
-        for(int i = 1; i <= n; i++){
-            if(i > 1) cout << ' ';
-            cout << ans[i];
-        }
-        cout << '\n';
-    }
-    return 0;
+    cout << '\n';
+  }
+  return 0;
 }

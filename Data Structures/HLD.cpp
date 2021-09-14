cd54638f71b06b657cda35bc6141ecda4d113699
ShahjalalShohag <33003206+ShahjalalShohag@users.noreply.github.com>
2021-09-14 16:35:00 +0600

    Update HLD.cpp

--- Data Structures/HLD.cpp
+++ Data Structures/HLD.cpp
@@ -3,144 +3,139 @@ using namespace std;
 
 const int N = 1e5 + 9, LG = 18, inf = 1e9 + 9;
 
-struct ST
-{
-    #define lc (n << 1)
-    #define rc ((n << 1) | 1)
-    int t[4 * N], lazy[4 * N];
-    ST()
-    {
-        fill(t, t + 4 * N, -inf);
-        fill(lazy, lazy + 4 * N, 0);
+struct ST {
+#define lc (n << 1)
+#define rc ((n << 1) | 1)
+  int t[4 * N], lazy[4 * N];
+  ST() {
+    fill(t, t + 4 * N, -inf);
+    fill(lazy, lazy + 4 * N, 0);
+  }
+  inline void push(int n, int b, int e) {
+    if(lazy[n] == 0) return;
+    t[n] = t[n] + lazy[n];
+    if(b != e) {
+      lazy[lc] = lazy[lc] + lazy[n];
+      lazy[rc] = lazy[rc] + lazy[n];
     }
-    inline void push(int n, int b, int e)
-    {
-        if(lazy[n] == 0) return;
-        t[n] = t[n] + lazy[n];
-        if(b != e){
-            lazy[lc] = lazy[lc] + lazy[n];
-            lazy[rc] = lazy[rc] + lazy[n];
-        }
-        lazy[n] = 0;
+    lazy[n] = 0;
+  }
+  inline int combine(int a, int b) {
+    return max(a, b); //merge left and right queries
+  }
+  inline void pull(int n) {
+    t[n] = max(t[lc], t[rc]); //merge lower nodes of the tree to get the parent node
+  }
+  void build(int n, int b, int e) {
+    if(b == e) {
+      t[n] = 0;
+      return;
     }
-    inline int combine(int a,int b)
-    {
-        return max(a, b); //merge left and right queries
+    int mid = (b + e) >> 1;
+    build(lc, b, mid);
+    build(rc, mid + 1, e);
+    pull(n);
+  }
+  void upd(int n, int b, int e, int i, int j, int v) {
+    push(n, b, e);
+    if(j < b || e < i) return;
+    if(i <= b && e <= j) {
+      lazy[n] += v;
+      push(n, b, e);
+      return;
     }
-    inline void pull(int n)
-    {
-        t[n] = max(t[lc], t[rc]); //merge lower nodes of the tree to get the parent node
-    }
-    void build(int n, int b, int e)
-    {
-        if(b == e){
-            t[n] = 0;
-            return;
-        }
-        int mid = (b + e) >> 1;
-        build(lc, b, mid);
-        build(rc, mid + 1, e);
-        pull(n);
-    }
-    void upd(int n, int b, int e, int i, int j, int v)
-    {
-        push(n, b, e);
-        if(j < b || e < i) return;
-        if(i <= b && e <= j) {
-            lazy[n] += v;
-            push(n, b, e);
-            return;
-        }
-        int mid = (b + e) >> 1;
-        upd(lc, b, mid, i, j, v);
-        upd(rc, mid + 1, e, i, j, v);
-        pull(n);
-    }
-    int query(int n, int b, int e, int i, int j)
-    {
-        push(n, b, e);
-        if(i > e || b > j) return -inf;
-        if(i <= b && e <= j) return t[n];
-        int mid = (b + e) >> 1;
-        return combine(query(lc, b, mid, i, j), query(rc, mid + 1, e, i, j));
-    }
-}t;
+    int mid = (b + e) >> 1;
+    upd(lc, b, mid, i, j, v);
+    upd(rc, mid + 1, e, i, j, v);
+    pull(n);
+  }
+  int query(int n, int b, int e, int i, int j) {
+    push(n, b, e);
+    if(i > e || b > j) return -inf;
+    if(i <= b && e <= j) return t[n];
+    int mid = (b + e) >> 1;
+    return combine(query(lc, b, mid, i, j), query(rc, mid + 1, e, i, j));
+  }
+} t;
 
 vector<int> g[N];
 int par[N][LG + 1], dep[N], sz[N];
 void dfs(int u, int p = 0) {
-    par[u][0] = p;
-    dep[u] = dep[p] + 1;
-    sz[u] = 1;
-    for (int i = 1; i <= LG; i++) par[u][i] = par[par[u][i - 1]][i - 1];
-    if (p) g[u].erase(find(g[u].begin(), g[u].end(), p));
-    for (auto &v: g[u]) if (v != p) {
-        dfs(v, u);
-        sz[u] += sz[v];
-        if(sz[v] > sz[g[u][0]]) swap(v, g[u][0]);
+  par[u][0] = p;
+  dep[u] = dep[p] + 1;
+  sz[u] = 1;
+  for (int i = 1; i <= LG; i++) par[u][i] = par[par[u][i - 1]][i - 1];
+  if (p) g[u].erase(find(g[u].begin(), g[u].end(), p));
+  for (auto &v : g[u]) if (v != p) {
+      dfs(v, u);
+      sz[u] += sz[v];
+      if(sz[v] > sz[g[u][0]]) swap(v, g[u][0]);
     }
 }
 int lca(int u, int v) {
-    if (dep[u] < dep[v]) swap(u, v);
-    for (int k = LG; k >= 0; k--) if (dep[par[u][k]] >= dep[v]) u = par[u][k];
-    if (u == v) return u;
-    for (int k = LG; k >= 0; k--) if (par[u][k] != par[v][k]) u = par[u][k], v = par[v][k];
-    return par[u][0];
+  if (dep[u] < dep[v]) swap(u, v);
+  for (int k = LG; k >= 0; k--) if (dep[par[u][k]] >= dep[v]) u = par[u][k];
+  if (u == v) return u;
+  for (int k = LG; k >= 0; k--) if (par[u][k] != par[v][k]) u = par[u][k], v = par[v][k];
+  return par[u][0];
 }
 int kth(int u, int k) {
-    assert(k >= 0);
-    for (int i = 0; i <= LG; i++) if (k & (1 << i)) u = par[u][i];
-    return u;
+  assert(k >= 0);
+  for (int i = 0; i <= LG; i++) if (k & (1 << i)) u = par[u][i];
+  return u;
 }
 int T, head[N], st[N], en[N];
 void dfs_hld(int u) {
-    st[u] = ++T;
-    for (auto v: g[u]) {
-        head[v] = (v == g[u][0] ? head[u] : v);
-        dfs_hld(v);
-    }
-    en[u] = T;
+  st[u] = ++T;
+  for (auto v : g[u]) {
+    head[v] = (v == g[u][0] ? head[u] : v);
+    dfs_hld(v);
+  }
+  en[u] = T;
 }
 int n;
 int query_up(int u, int v) {
-    int ans = -inf;
-    while(head[u] != head[v]){
-        ans = max(ans, t.query(1, 1, n, st[head[u]], st[u]));
-        u = par[head[u]][0];
-    }
-    ans = max(ans, t.query(1, 1, n, st[v], st[u]));
-    return ans;
+  int ans = -inf;
+  while(head[u] != head[v]) {
+    ans = max(ans, t.query(1, 1, n, st[head[u]], st[u]));
+    u = par[head[u]][0];
+  }
+  ans = max(ans, t.query(1, 1, n, st[v], st[u]));
+  return ans;
 }
 int query(int u, int v) {
-    int l = lca(u, v);
-    int ans = query_up(u, l);
-    if (v != l) ans = max(ans, query_up(v, kth(v, dep[v] - dep[l] - 1)));
-    return ans;
+  int l = lca(u, v);
+  int ans = query_up(u, l);
+  if (v != l) ans = max(ans, query_up(v, kth(v, dep[v] - dep[l] - 1)));
+  return ans;
 }
 int32_t main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
 
-    cin >> n;
-    for (int i = 1; i < n; i++) {
-        int u, v; cin >> u >> v;
-        g[u].push_back(v);
-        g[v].push_back(u);
+  cin >> n;
+  for (int i = 1; i < n; i++) {
+    int u, v;
+    cin >> u >> v;
+    g[u].push_back(v);
+    g[v].push_back(u);
+  }
+  dfs(1);
+  head[1] = 1;
+  dfs_hld(1);
+  int q;
+  cin >> q;
+  t.build(1, 1, n);
+  while (q--) {
+    string ty;
+    int u, v;
+    cin >> ty >> u >> v;
+    if (ty == "add") {
+      t.upd(1, 1, n, st[u], en[u], v);
+    } else {
+      cout << query(u, v) << '\n';
     }
-    dfs(1);
-    head[1] = 1;
-    dfs_hld(1);
-    int q; cin >> q;
-    t.build(1, 1, n);
-    while (q--) {
-        string ty; int u, v; cin >> ty >> u >> v;
-        if (ty == "add") {
-            t.upd(1, 1, n, st[u], en[u], v);
-        }
-        else {
-            cout << query(u, v) << '\n';
-        }
-    }
-    return 0;
+  }
+  return 0;
 }
 //https://www.hackerrank.com/challenges/subtrees-and-paths/problem

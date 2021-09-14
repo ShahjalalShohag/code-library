da878562a4d1bcf7c5347f089566f272decb23fa
ShahjalalShohag <33003206+ShahjalalShohag@users.noreply.github.com>
2021-09-14 17:00:54 +0600

    Update Monotonous Queue.cpp

--- Data Structures/Monotonous Queue.cpp
+++ Data Structures/Monotonous Queue.cpp
@@ -3,35 +3,30 @@ using namespace std;
 
 const int N = 3e5 + 9;
 
-struct monotonous_queue //max, stores strictly decreasing sequence of the current queue
-{
-    int a[N + 10], b[N + 10], l = 0, r = -1;
-    void push(int val)
-    {
-        int cnt = 0;
-        while(l <= r && a[r] <= val){
-            cnt += b[r] + 1;
-            r--;
-        }
-        a[++r] = val, b[r] = cnt;
-    };
-    int top()
-    {
-        return a[l];
+struct monotonous_queue { //max, stores strictly decreasing sequence of the current queue
+  int a[N + 10], b[N + 10], l = 0, r = -1;
+  void push(int val) {
+    int cnt = 0;
+    while(l <= r && a[r] <= val) {
+      cnt += b[r] + 1;
+      r--;
     }
-    void pop()
-    {
-        if(l > r) return;
-        if (b[l] > 0){
-            b[l] --;
-            return;
-        }
-        l++;
+    a[++r] = val, b[r] = cnt;
+  };
+  int top() {
+    return a[l];
+  }
+  void pop() {
+    if(l > r) return;
+    if (b[l] > 0) {
+      b[l] --;
+      return;
     }
+    l++;
+  }
 };
 
-int32_t main()
-{
+int32_t main() {
 
-    return 0;
+  return 0;
 }

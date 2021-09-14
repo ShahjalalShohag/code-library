8a4a64c2ec44dc479314a1d24979f83bacdbbdb6
ShahjalalShohag <33003206+ShahjalalShohag@users.noreply.github.com>
2021-09-14 14:46:45 +0600

    Update BIT.cpp

--- Data Structures/BIT.cpp
+++ Data Structures/BIT.cpp
@@ -4,41 +4,30 @@ using namespace std;
 const int N = 3e5 + 9;
 
 template <class T>
-struct BIT
-{
-    //1-indexed
-	int n;
-	vector<T> t;
-	void init(int _n)
-	{
-		n = _n;
-		t.assign(n,0);
-	}
-
-	T query(int i)
-	{
-		T ans = 0;
-		for(; i >= 1; i -= (i & -i)) ans += t[i];
-		return ans;
-	}
-
-	void upd(int i, T val)
-	{
-		if(i <= 0) return;
-		for(; i < n; i += (i & -i)) t[i] += val;
-	}
-
-	void upd(int l, int r, T val)
-	{
-	    upd(l, val);
-	    upd(r + 1, -val);
-	}
-
-	T query(int l, int r) { return query(r) - query(l - 1); }
+struct BIT { //1-indexed
+  int n; vector<T> t;
+  BIT() {}
+  BIT(int _n) {
+    n = _n; t.assign(n + 1, 0);
+  }
+  T query(int i) {
+    T ans = 0;
+    for (; i >= 1; i -= (i & -i)) ans += t[i];
+    return ans;
+  }
+  void upd(int i, T val) {
+    if (i <= 0) return;
+    for (; i <= n; i += (i & -i)) t[i] += val;
+  }
+  void upd(int l, int r, T val) {
+    upd(l, val);
+    upd(r + 1, -val);
+  }
+  T query(int l, int r) {
+    return query(r) - query(l - 1);
+  }
 };
+int32_t main() {
 
-int32_t main()
-{
-
-    return 0;
+  return 0;
 }

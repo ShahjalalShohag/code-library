c4f0cbeefc38a9b4b7b6ef987d17e3710d0dc7c6
ShahjalalShohag <33003206+ShahjalalShohag@users.noreply.github.com>
2021-09-14 14:14:57 +0600

    Update Segment Tree.cpp

--- Data Structures/Segment Tree.cpp
+++ Data Structures/Segment Tree.cpp
@@ -4,48 +4,46 @@ using namespace std;
 const int N = 3e5 + 9;
 
 int a[N];
-struct ST
-{
-    int t[4 * N];
-    ST(){ memset(t, 0, sizeof t); }
-    void build(int n, int b, int e)
-    {
-        if(b == e){
-            t[n] = a[b];
-            return;
-        }
-        int mid = (b + e) >> 1,  l = n << 1,  r = l | 1;
-        build(l, b, mid);
-        build(r, mid + 1, e);
-        t[n] = max(t[l], t[r]);
+struct ST {
+  int t[4 * N];
+  static const int inf = 1e9;
+  ST() {
+    memset(t, 0, sizeof t);
+  }
+  void build(int n, int b, int e) {
+    if (b == e) {
+      t[n] = a[b];
+      return;
     }
-    void upd(int n, int b, int e, int i, int x)
-    {
-        if(b > i || e < i) return;
-        if(b == e && b == i){
-            t[n] = x;
-            return;
-        }
-        int mid = (b + e) >> 1,  l = n << 1,  r = l | 1;
-        upd(l, b, mid, i, x);
-        upd(r, mid + 1, e, i, x);
-        t[n] = max(t[l], t[r]);
+    int mid = (b + e) >> 1, l = n << 1, r = l | 1;
+    build(l, b, mid);
+    build(r, mid + 1, e);
+    t[n] = max(t[l], t[r]);
+  }
+  void upd(int n, int b, int e, int i, int x) {
+    if (b > i || e < i) return;
+    if (b == e && b == i) {
+      t[n] = x;
+      return;
     }
-    int query(int n, int b, int e, int i, int j)
-    {
-        if(b > j || e < i) return -1e9;
-        if(b >= i && e <= j) return t[n];
-        int mid = (b + e) >> 1,  l = n << 1,  r = l | 1;
-        int L = query(l, b, mid, i, j);
-        int R = query(r, mid + 1, e, i, j);
-        return max(L, R);
-    }
+    int mid = (b + e) >> 1, l = n << 1, r = l | 1;
+    upd(l, b, mid, i, x);
+    upd(r, mid + 1, e, i, x);
+    t[n] = max(t[l], t[r]);
+  }
+  int query(int n, int b, int e, int i, int j) {
+    if (b > j || e < i) return -inf;
+    if (b >= i && e <= j) return t[n];
+    int mid = (b + e) >> 1, l = n << 1, r = l | 1;
+    int L = query(l, b, mid, i, j);
+    int R = query(r, mid + 1, e, i, j);
+    return max(L, R);
+  }
 }t;
 
-
-int32_t main()
-{
-
-    return 0;
+int32_t main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  
+  return 0;
 }
-

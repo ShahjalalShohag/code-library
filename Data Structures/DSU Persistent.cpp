e5d820c01f611e698f7d87c7bbaabcce15c94bd5
ShahjalalShohag <33003206+ShahjalalShohag@users.noreply.github.com>
2021-09-14 15:24:39 +0600

    Update DSU Persistent.cpp

--- Data Structures/DSU Persistent.cpp
+++ Data Structures/DSU Persistent.cpp
@@ -1,85 +1,146 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-const int N = 1e5 + 9, B = 320;
+const int N = 2e5 + 9;
 
-struct PDSU
-{
-	int c, dep[N], par[N];
-	stack<array<int, 4>> st;
-	PDSU() {}
-	void init(int n) {
-		c = n;
-		for(int i = 0; i <= n; i++) par[i] = i, dep[i] = 1;
-        st = stack<array<int, 4>>();
-	}
-	int root(int x) { return x == par[x] ? x : root(par[x]); }
-	bool same(int x, int y) { return root(x) == root(y); }
-	int size(int x) { return dep[root(x)]; }
-	int count() { return c; }
-	void unite(int x, int y) {
-		if((x = root(x)) == (y = root(y))) return;
-		st.push({x, dep[x], y, dep[y]});
-		if(dep[x] <= dep[y]) par[x] = y, dep[y] += dep[x];
-		else par[y] = x, dep[x] += dep[y];
-		c--;
-	}
-	void snapshot() { st.push({-1, -1, -1, -1}); }
-	void rollback() {
-		while(!st.empty()){
-			if(st.top()[0] == -1) return;
-			++c;
-			for(int i = 0; i < 3; i += 2) par[st.top()[i]] = st.top()[i], dep[st.top()[i]] = st.top()[i + 1];
-			st.pop();
-		}
-	}
+struct DSU {
+  vector<int> par, sz, w;
+  vector<array<int, 3>> op;
+  bool flag;
+  DSU() {}
+  DSU(int n) {
+    par.resize(n + 1);
+    sz.resize(n + 1);
+    w.resize(n + 1);
+    flag = true;
+    for (int i = 1; i <= n; i++) {
+      par[i] = i; 
+      sz[i] = 1; w[i] = 0;
+    }
+  }
+  bool is_bipartite() {
+    return flag;
+  }
+  pair<int, int> find(int u) {
+    int ans = 0;
+    while (par[u] != u)  {
+      ans ^= w[u];
+      u = par[u];
+    }
+    return make_pair(u, ans);
+  }
+  bool merge(int u, int v) {
+    auto pu = find(u), pv = find(v);
+    u = pu.first;
+    v = pv.first;
+    int last = flag;
+    int z = pu.second ^ pv.second ^ 1;
+    if (u == v) {
+      if (z) {
+        flag = false;
+      }
+      op.push_back({-1, -1, last});
+      return false;
+    }
+    if (sz[u] > sz[v]) {
+      swap(u, v);
+    }
+    op.push_back({u, w[u], last});
+    par[u] = v;
+    w[u] = z;
+    sz[v] += sz[u];
+    return true;
+  }
+  void undo() {
+    assert(!op.empty());
+    auto x = op.back();
+    flag = x[2];
+    int u = x[0];
+    if (u != -1) {
+      sz[par[u]] -= sz[u];
+      par[u] = u;
+      w[u] = x[1];
+    }
+    op.pop_back();
+  }
 };
-
-PDSU d;
-
-int u[N], v[N];
-int c = 0;
-bool ok(int l, int r)
-{
-    d.snapshot();
-    for(int i = l; i <= r; i++){
-        d.unite(u[i], v[i]);
-        c++;
+struct update {
+  bool type;
+  int x, y;
+  update(int _x, int _y) {
+    x = _x; y = _y;
+    type = 0;
+  }
+};
+struct DSUQueue {
+  DSU D;
+  vector<update> S;
+  DSUQueue(int n) {
+    D = DSU(n);
+  }
+  void push(update u) {
+    D.merge(u.x, u.y);
+    S.push_back(u);
+  }
+  void pop() {
+    assert(!S.empty());
+    vector<update> t[2];
+    do {
+      t[S.back().type].push_back(S.back());
+      S.pop_back();
+      D.undo();
+    } while (t[1].size() < t[0].size() && !S.empty());
+    if (t[1].empty()) {
+      for (auto &u : t[0]) {
+        u.type = 1;
+        push(u);
+      }
+    } 
+    else {
+      for (int i : {0, 1}) {
+        reverse(t[i].begin(), t[i].end());
+        for (auto &u : t[i]) push(u);
+      }
     }
-    int ok = 0;
-    if(d.c == 1) ok = 1;
-    d.rollback();
-    return ok;
-}
-int32_t main()
-{
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-
-    int n, m; cin >> n >> m;
-    for(int i = 1; i <= m; i++) cin >> u[i] >> v[i];
-    int r = 0;
-    long long ans = 0;
-    d.init(n);
-    int en = B - 1;
-    for(int l = 1; l <= m; l++){
-        if(r < l - 1){
-            r = l - 1;
-            d.init(n);
-        }
-        if(l % B == 0){
-            d.init(n);
-            en = l + B - 1;
-            for(int i = en + 1; i <= r; i++)  d.unite(u[i], v[i]), c++;
-        }
-        while(r + 1 <= m && !ok(l, min(r, en))){
-            r++;
-            if(r > en) d.unite(u[r], v[r]), c++;
-        }
-        if(ok(l, min(r, en))) ans += m - r + 1;
-        else break;
+    S.pop_back();
+    D.undo();
+  }
+  bool is_bipartite() {
+    return D.is_bipartite();
+  }
+};
+int u[N], v[N], a[N];
+int32_t main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  int n, m, q; cin >> n >> m >> q;
+  DSU P(n);
+  for (int i = 1; i <= m; i++) {
+    cin >> u[i] >> v[i];
+  }
+  DSUQueue D(n);
+  for (int i = 1; i <= m; i++) {
+    D.push(update(u[i], v[i]));
+  }
+  for (int l = 1, r = 1; l <= m; l++) {
+    while (r < l || (!D.is_bipartite() && r <= m)) {
+      D.pop();
+      ++r;
     }
-    cout << ans << '\n';
-    return 0;
+    if (D.is_bipartite()) a[l] = r - 1;
+    else a[l] = m + 1;
+    D.push(update(u[l], v[l]));
+  }
+  while (q--) {
+    int l, r; cin >> l >> r;
+    if (a[l] <= r) {
+      cout << "NO\n";
+    }
+    else {
+      cout << "YES\n";
+    }
+  }
+  return 0;
 }
-//https://www.hackerearth.com/practice/algorithms/graphs/depth-first-search/practice-problems/algorithm/connected-intervals-1/
+// https://codeforces.com/contest/1386/problem/C
+// https://codeforces.com/blog/entry/83467

99e0d93e19f0b52df4487afc3ee8a256b8564c7b
ShahjalalShohag <33003206+ShahjalalShohag@users.noreply.github.com>
2021-09-15 01:57:17 +0600

    Update String Hashing.cpp

--- Strings/String Hashing.cpp
+++ Strings/String Hashing.cpp
@@ -4,13 +4,15 @@ using namespace std;
 const int N = 1e6 + 9;
 
 int power(long long n, long long k, const int mod) {
-    int ans = 1 % mod; n %= mod; if (n < 0) n += mod;
-    while (k) {
-        if (k & 1) ans = (long long) ans * n % mod;
-        n = (long long) n * n % mod;
-        k >>= 1;
-    }
-    return ans;
+  int ans = 1 % mod;
+  n %= mod;
+  if (n < 0) n += mod;
+  while (k) {
+    if (k & 1) ans = (long long) ans * n % mod;
+    n = (long long) n * n % mod;
+    k >>= 1;
+  }
+  return ans;
 }
 
 const int MOD1 = 127657753, MOD2 = 987654319;
@@ -18,57 +20,62 @@ const int p1 = 137, p2 = 277;
 int ip1, ip2;
 pair<int, int> pw[N], ipw[N];
 void prec() {
-    pw[0] =  {1, 1};
-    for (int i = 1; i < N; i++) {
-        pw[i].first = 1LL * pw[i - 1].first * p1 % MOD1;
-        pw[i].second = 1LL * pw[i - 1].second * p2 % MOD2;
-    }
-    ip1 = power(p1, MOD1 - 2, MOD1);
-    ip2 = power(p2, MOD2 - 2, MOD2);
-    ipw[0] =  {1, 1};
-    for (int i = 1; i < N; i++) {
-        ipw[i].first = 1LL * ipw[i - 1].first * ip1 % MOD1;
-        ipw[i].second = 1LL * ipw[i - 1].second * ip2 % MOD2;
-    }
+  pw[0] =  {1, 1};
+  for (int i = 1; i < N; i++) {
+    pw[i].first = 1LL * pw[i - 1].first * p1 % MOD1;
+    pw[i].second = 1LL * pw[i - 1].second * p2 % MOD2;
+  }
+  ip1 = power(p1, MOD1 - 2, MOD1);
+  ip2 = power(p2, MOD2 - 2, MOD2);
+  ipw[0] =  {1, 1};
+  for (int i = 1; i < N; i++) {
+    ipw[i].first = 1LL * ipw[i - 1].first * ip1 % MOD1;
+    ipw[i].second = 1LL * ipw[i - 1].second * ip2 % MOD2;
+  }
 
 }
 struct Hashing {
-    int n; string s; // 0 - indexed
-    vector<pair<int, int>> hs; // 1 - indexed
-    Hashing() {}
-    Hashing(string _s) {
-        n = _s.size(); s = _s;
-        hs.emplace_back(0, 0);
-        for (int i = 0; i < n; i++) {
-            pair<int, int> p;
-            p.first = (hs[i].first + 1LL * pw[i].first * s[i] % MOD1) % MOD1;
-            p.second = (hs[i].second + 1LL * pw[i].second * s[i] % MOD2) % MOD2;
-            hs.push_back(p);
-        }
+  int n;
+  string s; // 0 - indexed
+  vector<pair<int, int>> hs; // 1 - indexed
+  Hashing() {}
+  Hashing(string _s) {
+    n = _s.size();
+    s = _s;
+    hs.emplace_back(0, 0);
+    for (int i = 0; i < n; i++) {
+      pair<int, int> p;
+      p.first = (hs[i].first + 1LL * pw[i].first * s[i] % MOD1) % MOD1;
+      p.second = (hs[i].second + 1LL * pw[i].second * s[i] % MOD2) % MOD2;
+      hs.push_back(p);
     }
-    pair<int, int> get_hash(int l, int r) { // 1 - indexed
-    	assert(1 <= l && l <= r && r <= n);
-        pair<int, int> ans;
-        ans.first = (hs[r].first - hs[l - 1].first + MOD1) * 1LL * ipw[l - 1].first % MOD1;
-        ans.second = (hs[r].second - hs[l - 1].second + MOD2) * 1LL * ipw[l - 1].second % MOD2;
-        return ans;
-    }
-    pair<int, int> get_hash() { return get_hash(1, n); }
+  }
+  pair<int, int> get_hash(int l, int r) { // 1 - indexed
+    assert(1 <= l && l <= r && r <= n);
+    pair<int, int> ans;
+    ans.first = (hs[r].first - hs[l - 1].first + MOD1) * 1LL * ipw[l - 1].first % MOD1;
+    ans.second = (hs[r].second - hs[l - 1].second + MOD2) * 1LL * ipw[l - 1].second % MOD2;
+    return ans;
+  }
+  pair<int, int> get_hash() {
+    return get_hash(1, n);
+  }
 };
 int32_t main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    prec();
-    int n;
-    while (cin >> n) {
-        string s, p; cin >> p >> s;
-        Hashing h(s);
-        auto hs = Hashing(p).get_hash();
-        for(int i = 1; i + n - 1 <= s.size(); i++) {
-            if (h.get_hash(i, i + n - 1) == hs) cout << i - 1 << '\n';
-        }
-        cout << '\n';
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  prec();
+  int n;
+  while (cin >> n) {
+    string s, p;
+    cin >> p >> s;
+    Hashing h(s);
+    auto hs = Hashing(p).get_hash();
+    for(int i = 1; i + n - 1 <= s.size(); i++) {
+      if (h.get_hash(i, i + n - 1) == hs) cout << i - 1 << '\n';
     }
-    return 0;
+    cout << '\n';
+  }
+  return 0;
 }
 // https://www.spoj.com/problems/NHAY/

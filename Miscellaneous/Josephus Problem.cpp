33e885e0ba9afbbd04a5663dcd233ef85e623426
ShahjalalShohag <33003206+ShahjalalShohag@users.noreply.github.com>
2021-09-15 12:25:10 +0600

    Create Josephus Problem.cpp

---
+++ Miscellaneous/Josephus Problem.cpp
@@ -0,0 +1,15 @@
+// n = total person
+// will kill every kth person, if k = 2, 2,4,6,...
+// returns the mth killed person
+ll josephus(ll n, ll k, ll m) {
+  m = n - m;
+  if (k <= 1)return n - m;
+  ll i = m;
+  while (i < n) {
+    ll r = (i - m + k - 2) / (k - 1);
+    if ((i + r) > n) r = n - i;
+    else if (!r) r = 1;
+    i += r;
+    m = (m + (r * k)) % i;
+  } return m + 1;
+}

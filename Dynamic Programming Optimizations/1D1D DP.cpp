dc3cf0d6d1406006b03ac716034d289d071a13c4
ShahjalalShohag <33003206+ShahjalalShohag@users.noreply.github.com>
2021-09-15 11:10:45 +0600

    Update 1D1D DP.cpp

--- Dynamic Programming Optimizations/1D1D DP.cpp
+++ Dynamic Programming Optimizations/1D1D DP.cpp
@@ -16,43 +16,44 @@ Complexity: O(nlogn)*/
 int n, s, t[N], f[N];
 ///the cost function, (i, x]
 int w(int i, int x) {
-	if (i >= x) return 1e9; //inf > w(0, n)
-	return s * (f[n] - f[i]) + t[x] * (f[x] - f[i]); 
+  if (i >= x) return 1e9; //inf > w(0, n)
+  return s * (f[n] - f[i]) + t[x] * (f[x] - f[i]);
 }
 int dp[N];
 int32_t main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	cin >> n >> s;
-    t[0] = 0; f[0] = 0;
-    for (int i=1; i <= n; i++) {
-        cin >> t[i] >> f[i];
-        t[i] += t[i-1];
-        f[i] += f[i-1];
-    }
-    dp[0] = 0;
-    vector<pair<int, int> > v; // (start pos, best k)
-    v.push_back(make_pair(0, 0));
-    for (int x = 1; x <= n; x++) {
-        int k = (--lower_bound(v.begin(), v.end(), make_pair(x + 1, 0)))->second;
-        dp[x] = dp[k] + w(k, x);
-        for (int i = (int)v.size() - 1; i >= 0; i--) {
-            int y = v[i].first, oldk = v[i].second;
-            if (y > x && dp[x] + w(x, y) < dp[oldk] + w(oldk, y)) v.pop_back();
-            else {
-                int l = y + 1, r = n + 1;
-                while (l < r) {
-                    int mid = (l + r) / 2;
-                    if (dp[x] + w(x, mid) < dp[oldk] + w(oldk, mid)) r = mid;
-                    else l = mid + 1;
-                }
-                if (r != n + 1) v.push_back(make_pair(r, x));
-                break;
-            }
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> n >> s;
+  t[0] = 0;
+  f[0] = 0;
+  for (int i = 1; i <= n; i++) {
+    cin >> t[i] >> f[i];
+    t[i] += t[i - 1];
+    f[i] += f[i - 1];
+  }
+  dp[0] = 0;
+  vector<pair<int, int> > v; // (start pos, best k)
+  v.push_back(make_pair(0, 0));
+  for (int x = 1; x <= n; x++) {
+    int k = (--lower_bound(v.begin(), v.end(), make_pair(x + 1, 0)))->second;
+    dp[x] = dp[k] + w(k, x);
+    for (int i = (int)v.size() - 1; i >= 0; i--) {
+      int y = v[i].first, oldk = v[i].second;
+      if (y > x && dp[x] + w(x, y) < dp[oldk] + w(oldk, y)) v.pop_back();
+      else {
+        int l = y + 1, r = n + 1;
+        while (l < r) {
+          int mid = (l + r) / 2;
+          if (dp[x] + w(x, mid) < dp[oldk] + w(oldk, mid)) r = mid;
+          else l = mid + 1;
         }
-        if (v.size() == 0) v.push_back(make_pair(0, x));
+        if (r != n + 1) v.push_back(make_pair(r, x));
+        break;
+      }
     }
-    cout << dp[n] << '\n';
-    return 0;
+    if (v.size() == 0) v.push_back(make_pair(0, x));
+  }
+  cout << dp[n] << '\n';
+  return 0;
 }
 //https://vjudge.net/problem/OpenJ_Bailian-1180

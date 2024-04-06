3ced9e1c5763738db260546695560871119e656e
Shahjalal Shohag <33003206+ShahjalalShohag@users.noreply.github.com>
2024-04-07 03:40:21 +0600

    Update Manachers.cpp

--- Strings/Manachers.cpp
+++ Strings/Manachers.cpp
@@ -1,49 +1,43 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main() {
-  int i, j, k, n, m;
-  string s;
-  cin >> s;
-  n = s.size();
-  vector<int> d1(n);  // maximum odd length palindrome centered at i
-  // here d1[i]=the palindrome has d1[i]-1 right characters from i
-  // e.g. for aba, d1[1]=2;
-  for (int i = 0, l = 0, r = -1; i < n; i++) {
-    int k = (i > r) ? 1 : min(d1[l + r - i], r - i);
-    while (0 <= i - k && i + k < n && s[i - k] == s[i + k]) {
-      k++;
+struct Manacher {
+	vector<int> p[2];
+  // p[1][i] = (max odd length palindrome centered at i) / 2 [floor division]
+  // p[0][i] = same for even, it considers the right center
+  // e.g. for s = "abbabba", p[1][3] = 3, p[0][2] = 2
+	Manacher(string s) {
+		int n = s.size();
+		p[0].resize(n + 1);
+		p[1].resize(n);
+		for (int z = 0; z < 2; z++) {
+			for (int i = 0, l = 0, r = 0; i < n; i++) {
+				int t = r - i + !z;
+				if (i < r) p[z][i] = min(t, p[z][l + t]);
+				int L = i - p[z][i], R = i + p[z][i] - !z;
+				while (L >= 1 && R + 1 < n && s[L - 1] == s[R + 1]) 
+					p[z][i]++, L--, R++;
+				if (R > r) l = L, r = R;
+			}
     }
-    d1[i] = k--;
-    if (i + k > r) {
-      l = i - k;
-      r = i + k;
-    }
+	}
+	bool is_palindrome(int l, int r) {
+		int mid = (l + r + 1) / 2, len = r - l + 1;
+		return 2 * p[len % 2][mid] + len % 2 >= len;
+	}
+};
+
+int32_t main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+  string s; cin >> s;
+  Manacher M(s);
+  int n = s.size();
+  for (int i = 0; i < n; i++) {
+    cout << 2 * M.p[1][i] + 1 << ' ';
+    if (i + 1 < n) cout << 2 * M.p[0][i + 1] << ' ';
   }
-  vector<int> d2(n);  // maximum even length palindrome centered at i
-  // here d2[i]=the palindrome has d2[i]-1 right characters from i
-  // e.g. for abba, d2[2]=2;
-  for (int i = 0, l = 0, r = -1; i < n; i++) {
-    int k = (i > r) ? 0 : min(d2[l + r - i + 1], r - i + 1);
-    while (0 <= i - k - 1 && i + k < n && s[i - k - 1] == s[i + k]) {
-      k++;
-    }
-    d2[i] = k--;
-    if (i + k > r) {
-      l = i - k - 1;
-      r = i + k ;
-    }
-  }
-  for(i = 0; i < n; i++) cout << d1[i] << ' ';
-  cout << endl;
-  for(i = 0; i < n; i++) cout << d2[i] << ' ';
-  cout << endl;
-  // number of palindromes
-  long long ans = 0;
-  for(i = 0; i < n; i++) {
-    ans += 1LL * d1[i];
-    ans += 1LL * d2[i];
-  }
-  cout << ans << endl;
-  return 0;
+  cout << '\n';
+	return 0;
 }
+// https://judge.yosupo.jp/problem/enumerate_palindromes

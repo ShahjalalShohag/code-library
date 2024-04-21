e6e83d6c3fcc907dcc2c490ffcfd3888cb3e7ac6
Shahjalal Shohag <33003206+ShahjalalShohag@users.noreply.github.com>
2024-04-22 05:21:32 +0600

    Update Centroid Decomposition.cpp

--- Data Structures/Centroid Decomposition.cpp
+++ Data Structures/Centroid Decomposition.cpp
@@ -7,62 +7,62 @@ vector<int> g[N];
 int sz[N];
 int tot, done[N], cenpar[N];
 void calc_sz(int u, int p) {
-	tot ++;
-	sz[u] = 1;
-	for (auto v : g[u]) {
-		if(v == p || done[v]) continue;
-		calc_sz(v, u);
-		sz[u] += sz[v];
-	}
+  tot ++;
+  sz[u] = 1;
+  for (auto v : g[u]) {
+    if(v == p || done[v]) continue;
+    calc_sz(v, u);
+    sz[u] += sz[v];
+  }
 }
 int find_cen(int u, int p) {
-	for (auto v : g[u]) {
-		if(v == p || done[v]) continue;
-		else if(sz[v] > tot / 2) return find_cen(v, u);
-	}
-	return u;
+  for (auto v : g[u]) {
+    if(v == p || done[v]) continue;
+    else if(sz[v] > tot / 2) return find_cen(v, u);
+  }
+  return u;
 }
 void decompose(int u, int pre) {
-	tot = 0;
-	calc_sz(u, pre);
-	int cen = find_cen(u, pre);
-	cenpar[cen] = pre;
-	done[cen] = 1;
-	for(auto v : g[cen]) {
-		if(v == pre || done[v]) continue;
-		decompose(v, cen);
-	}
+  tot = 0;
+  calc_sz(u, pre);
+  int cen = find_cen(u, pre);
+  cenpar[cen] = pre;
+  done[cen] = 1;
+  for(auto v : g[cen]) {
+    if(v == pre || done[v]) continue;
+    decompose(v, cen);
+  }
 }
 int dep[N];
 void dfs(int u, int p = 0) {
-	for(auto v : g[u]) {
-		if(v == p) continue;
-		dep[v] = dep[u] + 1;
-		dfs(v, u);
-	}
+  for(auto v : g[u]) {
+    if(v == p) continue;
+    dep[v] = dep[u] + 1;
+    dfs(v, u);
+  }
 }
 int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	int n;
-	cin >> n;
-	for(int i = 1; i < n; i++) {
-		int u, v;
-		cin >> u >> v;
-		g[u].push_back(v);
-		g[v].push_back(u);
-	}
-	decompose(1, 0);
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  int n;
+  cin >> n;
+  for(int i = 1; i < n; i++) {
+    int u, v;
+    cin >> u >> v;
+    g[u].push_back(v);
+    g[v].push_back(u);
+  }
+  decompose(1, 0);
 
   
-	for(int i = 1; i <= n; i++) g[i].clear();
+  for(int i = 1; i <= n; i++) g[i].clear();
   int root;
-	for(int i = 1; i <= n; i++) {
-		g[cenpar[i]].push_back(i);
+  for(int i = 1; i <= n; i++) {
+    g[cenpar[i]].push_back(i);
     g[i].push_back(cenpar[i]);
-		if (cenpar[i] == 0) root = i;
-	}
-	dfs(root);
-	for(int i = 1; i <= n; i++) cout << char(dep[i] + 'A') << ' ';
-	return 0;
+    if (cenpar[i] == 0) root = i;
+  }
+  dfs(root);
+  for(int i = 1; i <= n; i++) cout << char(dep[i] + 'A') << ' ';
+  return 0;
 }

e1d9175165cbade570f1b32a9d409d04aabd7f5b
Shahjalal Shohag <33003206+ShahjalalShohag@users.noreply.github.com>
2024-04-23 07:27:20 +0600

    Update Static to Dynamic Trick.cpp

--- Data Structures/Static to Dynamic Trick.cpp
+++ Data Structures/Static to Dynamic Trick.cpp
@@ -1,135 +1,96 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-const int N = 1e5 + 9;
-struct aho_corasick_static {
-  int cnt[N], link[N], psz;
-  map<char, int> to[N];
+const int LG = 20;
+// an example static DS which takes multiples integers
+// and in the end it processes those data once in >= O(n)
+// then it can query each time fast enough
+struct ds_static {
+  ds_static() {}
+	vector<int> v;
+	void insert(int x) {
+		v.push_back(x);
+	}
+
+	void process() {
+		sort(v.begin(), v.end());
+	}
+
+	// how many numbers <= x
+	int query(int x) {
+		return upper_bound(v.begin(), v.end(), x) - v.begin();
+	}
 
   void clear() {
-    for(int i = 0; i < psz; i++)
-      cnt[i] = 0, link[i] = -1, to[i].clear();
-
-    psz = 1;
-    link[0] = -1;
-    cnt[0] = 0;
-  }
-
-  aho_corasick_static() {
-    psz = N - 2;
-    clear();
-  }
-
-  void add_word(string s) {
-    int u = 0;
-    for(char c : s) {
-      if(!to[u].count(c)) to[u][c] = psz++;
-      u = to[u][c];
-    }
-
-    cnt[u]++;
-  }
-
-  void push_links() {
-    queue<int> Q;
-    int u, v, j;
-    char c;
-
-    Q.push(0);
-    link[0] = -1;
-
-    while(!Q.empty()) {
-      u = Q.front();
-      Q.pop();
-
-      for(auto it : to[u]) {
-        v = it.second;
-        c = it.first;
-        j = link[u];
-
-        while(j != -1 && !to[j].count(c)) j = link[j];
-        if(j != -1) link[v] = to[j][c];
-        else link[v] = 0;
-
-        cnt[v] += cnt[link[v]];
-        Q.push(v);
-      }
-    }
-  }
-
-  int get_count(string p) {
-    int u = 0, ans = 0;
-    for(char c : p) {
-      while(u != -1 && !to[u].count(c)) u = link[u];
-      if(u == -1) u = 0;
-      else u = to[u][c];
-      ans += cnt[u];
-    }
-
-    return ans;
+    v.clear();
   }
 };
 
-struct aho_corasick {
-  vector<string> li[20];
-  aho_corasick_static ac[20];
+// now we can make this static ds into a dynamic ds
+// means, it can query anytime (not necessarily only at the end)
+// we can do it by only adding another log factor
+struct ds_dynamic {
+	vector<int> li[LG];
+	ds_static ds[LG];
 
-  void clear() {
-    for(int i = 0; i < 20; i++) {
-      li[i].clear();
-      ac[i].clear();
-    }
-  }
+	void clear() {
+		for(int i = 0; i < LG; i++) {
+			li[i].clear();
+			ds[i].clear();
+		}
+	}
 
-  aho_corasick() {
-    clear();
-  }
+	ds_dynamic() {
+		clear();
+	}
 
-  void add_word(string s) {
-    int pos = 0;
-    for(int l = 0; l < 20; l++)
-      if(li[l].empty()) {
-        pos = l;
-        break;
-      }
-
-    li[pos].push_back(s);
-    ac[pos].add_word(s);
-
-    for(int bef = 0; bef < pos; bef++) {
-      ac[bef].clear();
-      for(string s2 : li[bef]) {
-        li[pos].push_back(s2);
-        ac[pos].add_word(s2);
-      }
-
-      li[bef].clear();
+	void insert(int x) {
+		int pos = 0;
+		for(int l = 0; l < LG; l++) {
+			if(li[l].empty()) {
+				pos = l;
+				break;
+			}
     }
 
-    ac[pos].push_links();
-  }
-  //sum of occurrences of all patterns in this string
-  int get_count(string s) {
-    int ans = 0;
-    for(int l = 0; l < 20; l++)
-      ans += ac[l].get_count(s);
+		li[pos].push_back(x);
+		ds[pos].insert(x);
 
-    return ans;
-  }
+		for(int bef = 0; bef < pos; bef++) {
+			ds[bef].clear();
+			for(int x2 : li[bef]) {
+				li[pos].push_back(x2);
+				ds[pos].insert(x2);
+			}
+
+			li[bef].clear();
+		}
+
+		ds[pos].process();
+	}
+
+  // how many numbers <= x
+	int query(int x) {
+		int ans = 0;
+		for(int l = 0; l < LG; l++) {
+			ans += ds[l].query(x);
+		}
+		return ans;
+	}
 };
 
-string s[N];
-aho_corasick aho;
+ds_dynamic ds;
 
 int main() {
-  int i, j, k, n, m;
-  cin >> n;
-  for(int i = 0; i < n; i++)
-    cin >> s[i];
-  aho.clear();
-  for(int i = 0; i < n; i++) {
-    aho.add_word(s[i]);
-    cout << aho.get_count("aaaaaasssaaa") << endl;
-  }
-  return 0;
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int n = 100000;
+  long long res_sum = 0;
+	for (int i = 1; i <= n; i++) {
+		int x = rand();
+		ds.insert(x);
+		res_sum += ds.query(rand());
+	}
+  cout << res_sum << '\n';
+	return 0;
 }

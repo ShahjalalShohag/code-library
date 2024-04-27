5b35a4c1c514032ad597c3f60af199d252330b91
Shahjalal Shohag <33003206+ShahjalalShohag@users.noreply.github.com>
2024-04-27 10:33:12 +0600

    Update Sieve.cpp

--- Number Theory/Sieve.cpp
+++ Number Theory/Sieve.cpp
@@ -1,16 +1,46 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-const int N = 1e5 + 9;
+const int N = 1e6 + 9;
 
-int spf[N];
 vector<int> primes;
+bool is_prime[N]; 
+// use bitset<N> is_prime; to have O(N/64) memory complexity
+// using bitset you can solve upto around N = 10^8
+void sieve_v0() {
+  for (int i = 2; i < N; i++) {
+    is_prime[i] = true;
+  }
+  for (int i = 2; i * i < N; i++) {
+    if (is_prime[i]) {
+      for (int j = i * i; j < N; j += i) {
+        is_prime[j] = false;
+      }
+    }
+  }
+  for (int i = 2; i < N; i++) {
+    if (is_prime[i]) {
+      primes.push_back(i);
+    }
+  }
+}
+
+// sieve with smallest prime factors (spf)
+int spf[N];
 void sieve() {
-  for(int i = 2; i < N; i++) {
-    if (spf[i] == 0) spf[i] = i, primes.push_back(i);
-    int sz = primes.size();
-    for (int j = 0; j < sz && i * primes[j] < N && primes[j] <= spf[i]; j++) {
-      spf[i * primes[j]] = primes[j];
+  for (int i = 2; i < N; i++) {
+    spf[i] = i;
+  }
+  for (int i = 2; i * i < N; i++) {
+    if (spf[i] == i) {
+      for (int j = i * i; j < N; j += i) {
+        spf[j] = min(spf[j], i);
+      }
+    }
+  }
+  for (int i = 2; i < N; i++) {
+    if (spf[i] == i) {
+      primes.push_back(i);
     }
   }
 }
@@ -18,6 +48,8 @@ void sieve() {
 int32_t main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
-  
+  // sieve_v0();
+  sieve();
+  cout << primes.size() << '\n';
   return 0;
 }

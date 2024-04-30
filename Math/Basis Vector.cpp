05de44fd1c232d1105620dbb90360c2d88290611
Shahjalal Shohag <33003206+ShahjalalShohag@users.noreply.github.com>
2024-04-30 16:07:43 +0600

    Update Basis Vector.cpp

--- Math/Basis Vector.cpp
+++ Math/Basis Vector.cpp
@@ -1,13 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+template<typename T = int, int B = 31>
 struct Basis {
-  static const int B = 30;
-  int a[B];
+  T a[B];
   Basis() {
     memset(a, 0, sizeof a);
   }
-  void insert(int x){
+  void insert(T x){
     for (int i = B - 1; i >= 0; i--) {
       if (x >> i & 1) {
         if (a[i]) x ^= a[i];
@@ -18,13 +18,13 @@ struct Basis {
       }
     }
   }
-  bool can(int x) {
+  bool can(T x) {
     for(int i = B - 1; i >= 0; i--) {
       x = min(x, x ^ a[i]);
     }
     return x == 0;
   }
-  int max_xor(int ans = 0) {
+  T max_xor(T ans = 0) {
     for(int i = B - 1; i >= 0; i--) {
       ans = max(ans, ans ^ a[i]);
     }
@@ -38,9 +38,9 @@ int32_t main() {
   int t, cs = 0; cin >> t;
   while (t--) {
     int n; cin >> n;
-    Basis B;
+    Basis<long long, 63> B;
     for (int i = 1; i <= n; i++) {
-      int x; cin >> x;
+      long long x; cin >> x;
       B.insert(x);
     }
     cout << "Case " << ++cs << ": " << B.max_xor() << '\n';

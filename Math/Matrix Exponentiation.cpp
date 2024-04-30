073649f6d61e09f084fc87443155c66a1807d136
Shahjalal Shohag <33003206+ShahjalalShohag@users.noreply.github.com>
2024-04-30 13:46:58 +0600

    Update Matrix Exponentiation.cpp

--- Math/Matrix Exponentiation.cpp
+++ Math/Matrix Exponentiation.cpp
@@ -67,7 +67,7 @@ struct Mat {
 int32_t main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
-  int n, k; cin >> n >> k;
+  int n; long long k; cin >> n >> k;
   Mat a(n, n);
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < n; j++) {

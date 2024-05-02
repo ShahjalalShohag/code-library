c635918613508b1a5284842965ba34aa876342c1
Shahjalal Shohag <33003206+ShahjalalShohag@users.noreply.github.com>
2024-05-02 17:28:43 +0600

    Update Palindromic Tree.cpp

--- Strings/Palindromic Tree.cpp
+++ Strings/Palindromic Tree.cpp
@@ -4,15 +4,11 @@ using namespace std;
 const int N = 3e5 + 9;
 
 /*
--> diff(v) = len(v) - len(link(v))
--> series link will lead from the vertex v to the vertex u corresponding
-   to the maximum suffix palindrome of v which satisfies diff(v) != diff(u)
--> path within series links to the root contains only O(log n) vertices
 -> cnt contains the number of palindromic suffixes of the node
 */
 struct PalindromicTree {
   struct node {
-    int nxt[26], len, st, en, link, diff, slink, cnt, oc;
+    int nxt[26], len, st, en, link, cnt, oc;
   };
   string s;
   vector<node> t;
@@ -26,9 +22,6 @@ struct PalindromicTree {
     sz = 2, last = 2;
     t[1].len = -1, t[1].link = 1;
     t[2].len = 0, t[2].link = 1;
-    t[1].diff = t[2].diff = 0;
-    t[1].slink = 1;
-    t[2].slink = 2;
   }
   int extend(int pos) { // returns 1 if it creates a new palindrome
     int cur = last, curlen = 0;
@@ -53,8 +46,6 @@ struct PalindromicTree {
     if (t[sz].len == 1) {
       t[sz].link = 2;
       t[sz].cnt = 1;
-      t[sz].diff = 1;
-      t[sz].slink = 2;
       return 1;
     }
     while (1) {
@@ -66,30 +57,10 @@ struct PalindromicTree {
       }
     }
     t[sz].cnt = 1 + t[t[sz].link].cnt;
-    t[sz].diff = t[sz].len - t[t[sz].link].len;
-    if (t[sz].diff == t[t[sz].link].diff) t[sz].slink = t[t[sz].link].slink;
-    else t[sz].slink = t[sz].link;
     return 1;
   }
   void calc_occurrences() {
     for (int i = sz; i >= 3; i--) t[t[i].link].oc += t[i].oc;
-  }
-  vector<array<int, 2>> minimum_partition() { //(even, odd), 1 indexed
-    int n = s.size();
-    vector<array<int, 2>> ans(n + 1, {0, 0}), series_ans(n + 5, {0, 0});
-    ans[0][1] = series_ans[2][1] = 1e9;
-    for (int i = 1; i <= n; i++) {
-      extend(i - 1);
-      for (int k = 0; k < 2; k++) {
-        ans[i][k] = 1e9;
-        for (int v = last; t[v].len > 0; v = t[v].slink) {
-          series_ans[v][!k] = ans[i - (t[t[v].slink].len + t[v].diff)][!k];
-          if (t[v].diff == t[t[v].link].diff) series_ans[v][!k] = min(series_ans[v][!k], series_ans[t[v].link][!k]);
-          ans[i][k] = min(ans[i][k], series_ans[v][!k] + 1);
-        }
-      }
-    }
-    return ans;
   }
 } t;
 
@@ -101,15 +72,8 @@ int32_t main() {
   PalindromicTree t(s);
   for (int i = 0; i < s.size(); i++) t.extend(i);
   t.calc_occurrences();
-  long long ans = 0;
+  long long ans = 0; // number of palindromes
   for (int i = 3; i <= t.sz; i++) ans += t.t[i].oc;
   cout << ans << '\n';
   return 0;
-  //auto ans = t.minimum_partition();
-  // for (int i = 1; i <= s.size(); i++) {
-  //     cout << (ans[i][1] == 1e9 ? -1 : ans[i][1]) << ' ';
-  //     cout << (ans[i][0] == 1e9 ? -2 : ans[i][0]) << '\n';
-  // }
-  return 0;
 }
-// https://acm.timus.ru/problem.aspx?space=1&num=2058

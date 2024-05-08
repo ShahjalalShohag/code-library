d422d8b985417c91b4d157cddffcf9e8dcf8e378
Shahjalal Shohag <33003206+ShahjalalShohag@users.noreply.github.com>
2024-05-08 18:03:32 +0600

    Create Binomial Coefficients.cpp

---
+++ Basics/Binomial Coefficients.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 1e6 + 9, mod = 1e9 + 7;
+
+int f[N], inv[N], finv[N];
+void prec() {
+	f[0] = 1;
+	for (int i = 1; i < N; i++) f[i] = 1LL * i * f[i - 1] % mod;
+	inv[1] = 1;
+	for (int i = 2; i < N; i++ ) {
+		inv[i] = (-(1LL * mod / i) * inv[mod % i] ) % mod;
+		inv[i] = (inv[i] + mod) % mod;
+	}
+	finv[0] = 1;
+	for (int i = 1; i < N; i++) finv[i] = 1LL * inv[i] * finv[i - 1] % mod;
+}
+int ncr(int n, int r) {
+	if (n < r || n < 0 || r < 0) return 0;
+	return 1LL * f[n] * finv[n - r] % mod * finv[r] % mod;
+}
+
+// void brute() {
+// 	for (int i = 0; i < N; i++) {
+// 	  C[i][0] = 1;
+// 	}
+// 	for (int i = 1; i < N; i++) {
+// 	  for (int j = 1; j <= i; j++) {
+// 	    C[i][j] = (C[i - 1][j] + C[i - 1][j - 1]) % mod;
+// 	  }
+// 	}
+// }
+
+int32_t main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	prec();
+	int q; cin >> q;
+	while (q--) {
+		int n, r; cin >> n >> r;
+		cout << ncr(n, r) << '\n';
+	}
+	return 0;
+}
+// https://cses.fi/problemset/task/1079/

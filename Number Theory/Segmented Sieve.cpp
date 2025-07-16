6e889bf934c32517bfed164ee3138a805adc9a92
Shahjalal Shohag <33003206+ShahjalalShohag@users.noreply.github.com>
2025-07-17 01:18:43 +0600

    Update Segmented Sieve.cpp

--- Number Theory/Segmented Sieve.cpp
+++ Number Theory/Segmented Sieve.cpp
@@ -1,53 +1,64 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> generatePrimesUpTo(int limit) {
-    vector<bool> isPrime(limit + 1, true);
-    isPrime[0] = isPrime[1] = false;
+#define ll long long
+
+// Generate all primes up to limit using sieve of eratosthenes
+vector<int> sieve(int limit) {
+    vector<bool> is_prime(limit + 1, true);
+    is_prime[0] = is_prime[1] = false;
     for (int p = 2; p * p <= limit; ++p) {
-        if (isPrime[p]) {
+        if (is_prime[p]) {
             for (int i = p * p; i <= limit; i += p) {
-                isPrime[i] = false;
+                is_prime[i] = false;
             }
         }
     }
     vector<int> primes;
     for (int p = 2; p <= limit; ++p) {
-        if (isPrime[p]) {
+        if (is_prime[p]) {
             primes.push_back(p);
         }
     }
     return primes;
 }
-// Generate all primes from m to n
-void segmentedSieve(int m, int n) {
-    if (m == 1) {
-        m++;
+
+// Generate all primes from l to r using segmented sieve in O((r - l) log (r) + sqrt(r))
+vector<ll> segmented_sieve(ll l, ll r) {
+    if (l == 1) {
+        l++;
     }
-    int limit = sqrt(n);
-    vector<int> primes = generatePrimesUpTo(limit);
-    vector<bool> isPrime(n - m + 1, true);
-    for (int p : primes) {
-        int start = max(p * p, m + ((p - m % p) % p));
-        for (int j = start; j <= n; j += p) {
-            isPrime[j - m] = false;
+    int limit = sqrtl(r);
+    while ((ll) limit * limit <= r) limit++;
+    while ((ll) limit * limit > r) limit--;
+    auto primes = sieve(limit);
+    vector<bool> is_prime(r - l + 1, true);
+    for (ll p : primes) {
+        ll start = max((ll)p * p, (ll)(l + p - 1) / p * p);
+        for (ll j = start; j <= r; j += p) {
+            is_prime[j - l] = false;
         }
     }
-    for (int i = m; i <= n; ++i) {
-        if (isPrime[i - m]) {
-            cout << i << endl;
+    vector<ll> vec;
+    for (ll i = l; i <= r; ++i) {
+        if (is_prime[i - l]) {
+            vec.push_back(i);
         }
     }
+    return vec;
 }
 
 int main() {
     int t;
     cin >> t;
     while (t--) {
-        int m, n;
-        cin >> m >> n;
-        segmentedSieve(m, n);
-        if (t > 0) cout << endl;
+        ll l, r;
+        cin >> l >> r;
+        auto primes = segmented_sieve(l, r);
+        for (auto p: primes) {
+            cout << p << '\n';
+        }
+        if (t > 0) cout << '\n';
     }
     return 0;
 }

cc8fdb69d9bfef1498b291fef6c2be0e87a3fbfc
Shahjalal Shohag <33003206+ShahjalalShohag@users.noreply.github.com>
2025-09-18 03:49:06 +0600

    Update Basis Vector.cpp

--- Math/Basis Vector.cpp
+++ Math/Basis Vector.cpp
@@ -1,50 +1,125 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+const int N = 3e5 + 9;
+using ll = long long;
+
 template<typename T = int, int B = 31>
 struct Basis {
-  T a[B];
-  Basis() {
-    memset(a, 0, sizeof a);
+  T basis[B]; int sz;
+  void clear() {
+    memset(basis, 0, sizeof basis);
+    sz = 0;
   }
-  void insert(T x){
+  Basis() {
+    clear();
+  }
+  void insert(T x) {
     for (int i = B - 1; i >= 0; i--) {
       if (x >> i & 1) {
-        if (a[i]) x ^= a[i];
+        if (basis[i]) x ^= basis[i];
         else {
-          a[i] = x;
+          basis[i] = x;
+          sz++;
           break;
         }
       }
     }
   }
+  // is there any subset having xor = x
   bool can(T x) {
     for(int i = B - 1; i >= 0; i--) {
-      x = min(x, x ^ a[i]);
+      x = min(x, x ^ basis[i]);
     }
     return x == 0;
   }
-  T max_xor(T ans = 0) {
+  // max subset xor after xoring it with x
+  T max_xor(T x = 0) { 
     for(int i = B - 1; i >= 0; i--) {
-      ans = max(ans, ans ^ a[i]);
+      x = max(x, x ^ basis[i]);
+    }
+    return x;
+  }
+  // kth smallest subset xor, 1st is 0
+  T kth(T k) {
+    if (k < 1 || k > ((T)1 << sz)) {
+      return -1;
+    }
+    T x = 0;
+    T cnt = ((T)1 << sz);
+    for (int i = B - 1; i >= 0; i--) {
+      if (basis[i]) {
+        if (k > (cnt >> 1)) {
+          // set the ith bit
+          if (!(x >> i & 1)) {
+            x ^= basis[i];
+          }
+          k -= (cnt >> 1);
+        } else {
+          // unset the ith bit
+          if (x >> i & 1) {
+            x ^= basis[i];
+          }
+        }
+        cnt >>= 1;
+      }
+    }
+    return x;
+  }
+  // number of subsets having xor < x
+  T count_lt(T x) {
+    if (x < 0) {
+      return 0;
+    }
+    T ans = 0;
+    T cnt = ((T)1 << sz);
+    T mask = 0;
+    for (int i = B - 1; i >= 0; i--) {
+      // at this stage, all prev > i th bits in mask and x are the same
+      if (basis[i]) {
+        if (x >> i & 1) {
+          ans += (cnt >> 1);
+          if (!(mask >> i & 1)) {
+            mask ^= basis[i];
+          }
+        } else {
+          if (mask >> i & 1) {
+            mask ^= basis[i];
+          }
+        }
+        cnt >>= 1;
+      } else {
+        if ((x >> i & 1) != (mask >> i & 1)) {
+          if (x >> i & 1) {
+            return ans + cnt;
+          } else {
+            return ans; 
+          }
+        }
+      }
     }
     return ans;
   }
 };
 
+void solve() {
+  int q; cin >> q;
+  Basis B;
+  while (q--) {
+    int ty, k; cin >> ty >> k;
+    if (ty == 1) B.insert(k);
+    else cout << B.kth(k) << '\n';
+  }
+}
+
 int32_t main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
-  int t, cs = 0; cin >> t;
+  int t = 1;
+  // cin >> t;
   while (t--) {
-    int n; cin >> n;
-    Basis<long long, 63> B;
-    for (int i = 1; i <= n; i++) {
-      long long x; cin >> x;
-      B.insert(x);
-    }
-    cout << "Case " << ++cs << ": " << B.max_xor() << '\n';
+    solve();
   }
   return 0;
 }
-// https://lightoj.com/problem/maximum-subset-sum
+// https://codeforces.com/group/qcIqFPYhVr/contest/203881/problem/S
